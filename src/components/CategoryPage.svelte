7042caa0717fed4879f0e80e32f1bf0243586088
Valentina <vmilitaru28@gmail.com>
2022-03-18 11:12:38 +0000

    removes the radio button component and links the CensusTable component with the vizStore

--- src/components/CategoryPage.svelte
+++ src/components/CategoryPage.svelte
@@ -5,7 +5,7 @@
   import { mapStore } from "../stores/stores";
   import { fetchVizData } from "../data/fetchVizData";
   import { getCodesForCategory } from "../helpers/categoryHelper";
-  import RadioButton from '../components/RadioButton.svelte';
+  import CensusTable from './CensusTable.svelte';
   import topics from '../data/curation'
 
   $: params = $page.params;
@@ -13,12 +13,6 @@
   $: topic = topics.find(t => t.slug === topicSlug);
   $: variableSlug = params.variable;
   $: variable = topic.variables.find(v => v.slug === variableSlug);
-  $: classification = {
-    name: "Default",
-    desc: "Default classification",
-    categories: variable.categories 
-  }
-  $: categorySlug = params.category;
   $: search = $page.url.search;
 
   $: if ($mapStore) {
@@ -37,32 +31,7 @@
   </span>
 </div>
 
-<div class="px-6">
-
-  <h1 class="text-3xl mb-0.5">{variable.name}</h1>
-  <div class="mb-6 flex gap-2">
-    <div>
-      {variable.desc}
-    </div>
-  </div>
-
-  <div class="flex flex-col text-lg last:border-b-[1px] border-b-red">
-    {#each classification.categories as category}
-      <a href={`/2021/${topicSlug}/${variableSlug}/default/${category.slug}${search}`} 
-        class="flex gap-2 items-center p-2 border-t-[1px] border-t-slate-300 cursor-pointer 
-          {category.slug === categorySlug ? 'bg-onspale' : ''}">
-        <RadioButton selected={category.slug === categorySlug} />
-        {category.name}
-      </a>
-    {/each}
-  </div>
-  <div class="h-[1px] border-t-[1px] mb-6 border-t-slate-300"></div>
-
-  <div class="mb-6">
-    Change the <a class="hyperlink" href={`/2021/${topic.slug}/${variableSlug}${search}`}>number of categories</a>.
-  </div>
-
-</div>
+  <CensusTable />
 
 <!--
 <div class="p-6 font-mono text-sm bg-onspale mb-6">

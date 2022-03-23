dd73b7296c55c094eff2d3ddf584d4ad705fafda
Patrick Fleming <patrickjosephfleming@gmail.com>
2022-03-23 08:04:31 +0000

    reverts formatting

--- src/components/CensusPanel.svelte
+++ src/components/CensusPanel.svelte
@@ -1,90 +1,88 @@
 <script lang="ts">
-	import { buildHyperlink } from '../helpers/buildHyperlinkHelper';
-	import Button from './Button.svelte';
-	import SearchBox from './SearchBox.svelte';
-	import TopicList from './TopicList.svelte';
+  import { buildHyperlink } from "../helpers/buildHyperlinkHelper";
+  import Button from "./Button.svelte";
+  import SearchBox from "./SearchBox.svelte";
+  import TopicList from "./TopicList.svelte";
 
-	let suggestionsVisible = false;
+  let suggestionsVisible = false;
 </script>
 
 <div class="tw-p-6 tw-pt-8 tw-bg-onspale">
-	<SearchBox name="search-census" />
-	<div class="tw-pt-6">
-		<div class="tw-flex tw-items-center tw-gap-1">
-			<svg
-				class="tw-w-4 tw-h-4 tw-relative {suggestionsVisible
-					? 'tw-rotate-90 -tw-left-[0.1rem] tw-top-[0.1rem]'
-					: ''}"
-				viewBox="0 0 8 13"
-				xmlns="http://www.w3.org/2000/svg"
-				focusable="false"
-				fill="currentColor"
-				><path
-					d="M5.74,14.28l-.57-.56a.5.5,0,0,1,0-.71h0l5-5-5-5a.5.5,0,0,1,0-.71h0l.57-.56a.5.5,0,0,1,.71,0h0l5.93,5.93a.5.5,0,0,1,0,.7L6.45,14.28a.5.5,0,0,1-.71,0Z"
-					transform="translate(-5.02 -1.59)"
-				/></svg
-			>
-			<button class="tw-hyperlink" on:click={() => (suggestionsVisible = !suggestionsVisible)}>
-				Show me suggestions
-			</button>
-		</div>
-		{#if suggestionsVisible}
-			<div class="tw-mt-3 tw-border-l-4 tw-border-l-gray-400 tw-p-3">
-				<ul class="tw-list-disc tw-list-inside">
-					<li class="tw-pb-1">
-						Which areas have the
-						<a
-							href={buildHyperlink({
-								topic: 'housing',
-								variable: 'owned-or-renting',
-								category: 'shared-ownership'
-							})}
-							class="tw-hyperlink"
-						>
-							highest levels of shared ownership properties</a
-						>?
-					</li>
-					<li class="tw-pb-1">
-						Which areas of the country are the
-						<a
-							href={buildHyperlink({
-								topic: 'health',
-								variable: 'general-health',
-								category: 'very-good'
-							})}
-							class="tw-hyperlink"
-						>
-							most healthy</a
-						>?
-					</li>
-					<li class="tw-pb-1">
-						Where are the
-						<a
-							href={buildHyperlink({
-								topic: 'housing',
-								variable: 'heating',
-								category: 'no-central-heating'
-							})}
-							class="tw-hyperlink"
-						>
-							highest levels of homes without central heating</a
-						>?
-					</li>
-				</ul>
-				<div class="tw-mt-5">
-					<Button
-						colour="secondary"
-						click={() => {
-							suggestionsVisible = false;
-						}}
-					>
-						Hide this
-					</Button>
-				</div>
-			</div>
-		{/if}
-	</div>
+  <SearchBox name="search-census" />
+  <div class="tw-pt-6">
+    <div class="tw-flex tw-items-center tw-gap-1">
+      <svg
+        class="tw-w-4 tw-h-4 tw-relative {suggestionsVisible ? 'tw-rotate-90 -tw-left-[0.1rem] tw-top-[0.1rem]' : ''}"
+        viewBox="0 0 8 13"
+        xmlns="http://www.w3.org/2000/svg"
+        focusable="false"
+        fill="currentColor"
+        ><path
+          d="M5.74,14.28l-.57-.56a.5.5,0,0,1,0-.71h0l5-5-5-5a.5.5,0,0,1,0-.71h0l.57-.56a.5.5,0,0,1,.71,0h0l5.93,5.93a.5.5,0,0,1,0,.7L6.45,14.28a.5.5,0,0,1-.71,0Z"
+          transform="translate(-5.02 -1.59)"
+        /></svg
+      >
+      <button class="tw-hyperlink" on:click={() => (suggestionsVisible = !suggestionsVisible)}>
+        Show me suggestions
+      </button>
+    </div>
+    {#if suggestionsVisible}
+      <div class="tw-mt-3 tw-border-l-4 tw-border-l-gray-400 tw-p-3">
+        <ul class="tw-list-disc tw-list-inside">
+          <li class="tw-pb-1">
+            Which areas have the
+            <a
+              href={buildHyperlink({
+                topic: "housing",
+                variable: "owned-or-renting",
+                category: "shared-ownership",
+              })}
+              class="tw-hyperlink"
+            >
+              highest levels of shared ownership properties</a
+            >?
+          </li>
+          <li class="tw-pb-1">
+            Which areas of the country are the
+            <a
+              href={buildHyperlink({
+                topic: "health",
+                variable: "general-health",
+                category: "very-good",
+              })}
+              class="tw-hyperlink"
+            >
+              most healthy</a
+            >?
+          </li>
+          <li class="tw-pb-1">
+            Where are the
+            <a
+              href={buildHyperlink({
+                topic: "housing",
+                variable: "heating",
+                category: "no-central-heating",
+              })}
+              class="tw-hyperlink"
+            >
+              highest levels of homes without central heating</a
+            >?
+          </li>
+        </ul>
+        <div class="tw-mt-5">
+          <Button
+            colour="secondary"
+            click={() => {
+              suggestionsVisible = false;
+            }}
+          >
+            Hide this
+          </Button>
+        </div>
+      </div>
+    {/if}
+  </div>
 </div>
 <div class="tw-p-6 ">
-	<TopicList />
+  <TopicList />
 </div>
--- src/components/SearchBox.svelte
+++ src/components/SearchBox.svelte
@@ -1,98 +1,98 @@
 <script lang="ts">
-	import Badge from './Badge.svelte';
-	import SearchBoxItem from './SearchBoxItem.svelte';
-	import { highlightText, searchCensus } from '../helpers/searchCensusHelper';
-	import { buildHyperlink } from '../helpers/buildHyperlinkHelper';
+  import Badge from "./Badge.svelte";
+  import SearchBoxItem from "./SearchBoxItem.svelte";
+  import { highlightText, searchCensus } from "../helpers/searchCensusHelper";
+  import { buildHyperlink } from "../helpers/buildHyperlinkHelper";
 
-	export let name: string;
-	let val = '';
-	$: results = searchCensus(val);
+  export let name: string;
+  let val = "";
+  $: results = searchCensus(val);
 </script>
 
 <div class="">
-	<div class="tw-flex tw-max-w-[30rem]">
-		<input
-			id="{name}-input"
-			{name}
-			type="search"
-			autocomplete="off"
-			class="tw-flex tw-items-center tw-justify-center tw-h-12 tw-p-2 tw-w-full tw-border-l-2 tw-border-t-2 tw-border-b-2 tw-border-black focus:tw-border-4 tw-custom-ring"
-			bind:value={val}
-		/>
-		<button tabindex="-1" type="submit" class="tw-bg-onsblue tw-px-3">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				class="tw-h-6 tw-w-6 tw-text-white "
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="3"
-					d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-				/>
-			</svg>
-		</button>
-	</div>
+  <div class="tw-flex tw-max-w-[30rem]">
+    <input
+      id="{name}-input"
+      {name}
+      type="search"
+      autocomplete="off"
+      class="tw-flex tw-items-center tw-justify-center tw-h-12 tw-p-2 tw-w-full tw-border-l-2 tw-border-t-2 tw-border-b-2 tw-border-black focus:tw-border-4 tw-custom-ring"
+      bind:value={val}
+    />
+    <button tabindex="-1" type="submit" class="tw-bg-onsblue tw-px-3">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="tw-h-6 tw-w-6 tw-text-white "
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="3"
+          d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+        />
+      </svg>
+    </button>
+  </div>
 
-	{#if val && val.length > 2}
-		<div class="tw-relative">
-			<div
-				class="tw-absolute tw-left-0 tw-right-0 tw-z-50 tw-max-h-[30rem] tw-overflow-y-auto tw-p-3 tw-pt-2 tw-border-[1px] tw-border-slate-600 tw-bg-white"
-			>
-				<ul>
-					{#each results.topics as t}
-						<SearchBoxItem link={buildHyperlink({ topic: t.slug })}>
-							<div class="tw-flex tw-items-center tw-gap-2">
-								<Badge className="tw-bg-slate-500">TOPIC</Badge>
-								<div class="tw-text-xl">{@html highlightText(t.name, val)}</div>
-							</div>
-							<div class="">
-								{@html highlightText(t.desc, val)}
-							</div>
-						</SearchBoxItem>
-					{/each}
-					{#each results.variables as v}
-						<SearchBoxItem
-							link={buildHyperlink({
-								topic: v.topic.slug,
-								variable: v.variable.slug
-							})}
-						>
-							<div class="tw-flex tw-items-center tw-gap-2">
-								<Badge className="tw-bg-slate-500">VARIABLE</Badge>
-								<div class="tw-text-xl">{@html highlightText(v.variable.name, val)}</div>
-							</div>
-							<div class="">
-								{@html highlightText(v.variable.desc, val)}
-							</div>
-						</SearchBoxItem>
-					{/each}
-					{#each results.categories as c}
-						<SearchBoxItem
-							link={buildHyperlink({
-								topic: c.topic.slug,
-								variable: c.variable.variable.slug,
-								category: c.category.slug
-							})}
-						>
-							<div class="tw-flex tw-items-center tw-gap-2">
-								<Badge className="tw-bg-slate-500">CATEGORY</Badge>
-								<div class="tw-text-xl">{@html highlightText(c.category.name, val)}</div>
-							</div>
-							<div class="">
-								in variable {c.variable.variable.name}
-							</div>
-						</SearchBoxItem>
-					{/each}
-				</ul>
-				<div class="tw-pt-3 tw-pb-1 tw-px-1">
-					{results.topics.length + results.variables.length + results.categories.length}
-					results
-				</div>
-			</div>
-		</div>
-	{/if}
+  {#if val && val.length > 2}
+    <div class="tw-relative">
+      <div
+        class="tw-absolute tw-left-0 tw-right-0 tw-z-50 tw-max-h-[30rem] tw-overflow-y-auto tw-p-3 tw-pt-2 tw-border-[1px] tw-border-slate-600 tw-bg-white"
+      >
+        <ul>
+          {#each results.topics as t}
+            <SearchBoxItem link={buildHyperlink({ topic: t.slug })}>
+              <div class="tw-flex tw-items-center tw-gap-2">
+                <Badge className="tw-bg-slate-500">TOPIC</Badge>
+                <div class="tw-text-xl">{@html highlightText(t.name, val)}</div>
+              </div>
+              <div class="">
+                {@html highlightText(t.desc, val)}
+              </div>
+            </SearchBoxItem>
+          {/each}
+          {#each results.variables as v}
+            <SearchBoxItem
+              link={buildHyperlink({
+                topic: v.topic.slug,
+                variable: v.variable.slug,
+              })}
+            >
+              <div class="tw-flex tw-items-center tw-gap-2">
+                <Badge className="tw-bg-slate-500">VARIABLE</Badge>
+                <div class="tw-text-xl">{@html highlightText(v.variable.name, val)}</div>
+              </div>
+              <div class="">
+                {@html highlightText(v.variable.desc, val)}
+              </div>
+            </SearchBoxItem>
+          {/each}
+          {#each results.categories as c}
+            <SearchBoxItem
+              link={buildHyperlink({
+                topic: c.topic.slug,
+                variable: c.variable.variable.slug,
+                category: c.category.slug,
+              })}
+            >
+              <div class="tw-flex tw-items-center tw-gap-2">
+                <Badge className="tw-bg-slate-500">CATEGORY</Badge>
+                <div class="tw-text-xl">{@html highlightText(c.category.name, val)}</div>
+              </div>
+              <div class="">
+                in variable {c.variable.variable.name}
+              </div>
+            </SearchBoxItem>
+          {/each}
+        </ul>
+        <div class="tw-pt-3 tw-pb-1 tw-px-1">
+          {results.topics.length + results.variables.length + results.categories.length}
+          results
+        </div>
+      </div>
+    </div>
+  {/if}
 </div>

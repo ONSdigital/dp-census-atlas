9f82f8adc102571bff28d8c633b4c415448ab8a4
Valentina <vmilitaru28@gmail.com>
2022-03-23 17:39:29 +0000

    adds the new Heading component to the Home and Topic pages

--- src/components/HomePage.svelte
+++ src/components/HomePage.svelte
@@ -1,5 +1,8 @@
 <script>
+  import { _ } from "svelte-i18n";
   import CensusPanel from "./CensusPanel.svelte";
+  import Heading from "./Heading.svelte";
 </script>
 
+<Heading serviceTitle={$_("heading.homePage.serviceTitle")} description={$_("heading.homePage.description")} />
 <CensusPanel />

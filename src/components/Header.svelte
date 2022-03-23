b27afc4416754546bfc59c133869f2a72057a3d5
Valentina <vmilitaru28@gmail.com>
2022-03-23 17:34:06 +0000

    updates the Heading component to meet the V4 design and removes the Explore census 2021 div from the Header component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,4 +7,3 @@
   <span class="tw-p-y-0.5 tw-px-2 tw-bg-black tw-text-white tw-font-bold">ALPHA</span>
   <span class="tw-p-y-0.5 tw-px-2 tw-bg-onsgreen tw-text-white tw-font-bold">POC</span>
 </div>
-<div class="xl:tw-hidden tw-p-1 tw-px-2 tw-bg-onsblue tw-text-white tw-text-lg">Explore Census 2021</div>

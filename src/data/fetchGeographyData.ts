195a67f98de9cad7b05c680d857870a252e4fba2
Vivian Allen <vivian.allen@methods.co.uk>
2022-03-23 16:02:35 +0000

    Stop asking geo endpoint for england and wales data (not available)
    Refactor fetchGeographyData.fetchGeographyLookup to return
    spatialHelper.defaultGeography (England and Wales) if asked for
    something that matches, RATHER than ask the API for it.
    
    This change needed as the back end does not hold geometry data for
    England and Wales.

--- src/data/fetchGeographyData.ts
+++ src/data/fetchGeographyData.ts
@@ -1,6 +1,7 @@
 import * as dsv from "d3-dsv"; // https://github.com/d3/d3/issues/3469
 import { selectedGeographyStore } from "../stores/stores";
 import type { GeographyLookupProps } from "../types";
+import { defaultGeography } from "../helpers/spatialHelper";
 
 const apiBaseUrl = `https://cep5lmkia0.execute-api.eu-west-1.amazonaws.com/dev`;
 
@@ -29,6 +30,11 @@ export const fetchGeographyData = async (args: { totalCode: string; categoryCode
 };
 
 export const fetchGeographyLookup = async (location: string, useCode = true) => {
+  // return defaultGeography if the name or code matches...
+  if ((useCode && defaultGeography.meta.code == location) || defaultGeography.meta.code == location) {
+    return JSON.stringify(defaultGeography);
+  }
+  // otherwise query API
   const url = `${apiBaseUrl}/geo/2011?${useCode ? "geocode" : "geoname"}=${location}`;
   const response = await fetch(url);
   const data = await response.text();

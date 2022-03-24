1955cc2a97991fda97bd87405d41034c623cef29
Valentina <vmilitaru28@gmail.com>
2022-03-24 17:39:11 +0000

    adds the serviceTitle text for the TopicsPage heading

--- src/components/TopicsPage.svelte
+++ src/components/TopicsPage.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+  import { _ } from "svelte-i18n";
   import Heading from "./Heading.svelte";
   import TopicList from "./TopicList.svelte";
+  import { selectedGeographyStore } from "../stores/stores";
+
+  $: selectedGeographyDisplayName = $selectedGeographyStore?.displayName;
 </script>
 
-<Heading />
+<Heading
+  serviceTitle={$_("heading.topicsPage.serviceTitle") +
+    `${selectedGeographyDisplayName ? selectedGeographyDisplayName : "England and Wales"}`}
+/>
 <TopicList />

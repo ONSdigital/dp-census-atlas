133104dcd2c23dc0dd0f7d6c43f5b2ee65031bee
Valentina <vmilitaru28@gmail.com>
2022-03-24 16:56:12 +0000

    creates a TopicsPage component and adds a topics route file to render the TopicsPage component

---
+++ src/components/TopicsPage.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import Heading from "./Heading.svelte";
+  import TopicList from "./TopicList.svelte";
+</script>
+
+<Heading />
+<TopicList />
---
+++ src/routes/2021/topics.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import TopicsPage from "../../components/TopicsPage.svelte";
+</script>
+
+<TopicsPage />

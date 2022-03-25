62f43b5e1f572a7063f95cd4430d06206d8bc256
Valentina <vmilitaru28@gmail.com>
2022-03-25 11:16:44 +0000

    adds location overview component

---
+++ src/components/LocationOverview.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let allPeopleTotal: string, allHouseholdsTotal: string;
+</script>
+
+<div class="ons-grid explore-container">
+  <div class="ons-grid__col ons-col-6@m">
+    <div class="ons-pl-grid-col">
+      <h3 class="ons-u-fs-s explore-sub-title">All people (usual residents)</h3>
+      <p class="ons-u-fs-l explore-data">{allPeopleTotal}</p>
+    </div>
+  </div>
+  <div class="ons-grid__col ons-col-6@m">
+    <div class="ons-pl-grid-col">
+      <h3 class="ons-u-fs-s explore-sub-title">All households</h3>
+      <p class="ons-u-fs-l explore-data">{allHouseholdsTotal}</p>
+    </div>
+  </div>
+</div>

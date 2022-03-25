95f5001d6308b178d49edaf0c3d85534c9e52cf4
Valentina <vmilitaru28@gmail.com>
2022-03-25 11:18:03 +0000

    creates a LocationPage component and adds a location route to render the component

---
+++ src/components/LocationPage.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Heading from "./Heading.svelte";
+
+  import { selectedGeographyStore } from "../stores/stores";
+
+  $: selectedGeographyDisplayName = $selectedGeographyStore?.displayName;
+</script>
+
+<Heading serviceTitle={selectedGeographyDisplayName ? selectedGeographyDisplayName : "England and Wales"} />
---
+++ src/routes/2021/location.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import LocationPage from "../../components/LocationPage.svelte";
+</script>
+
+<LocationPage />

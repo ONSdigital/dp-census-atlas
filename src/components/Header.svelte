f63056753af288e5a4a7277206b323f2a06eb21e
Patrick Fleming <patrickjosephfleming@gmail.com>
2022-03-28 14:58:04 +0100

    adds header with partial styling

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -2,8 +2,13 @@
   import Logo from "./Logo.svelte";
 </script>
 
-<div class="tw-p-1 tw-flex tw-items-center tw-gap-3">
-  <Logo />
-  <span class="tw-p-y-0.5 tw-px-2 tw-bg-black tw-text-white tw-font-bold">ALPHA</span>
-  <span class="tw-p-y-0.5 tw-px-2 tw-bg-onsgreen tw-text-white tw-font-bold">POC</span>
+<div class="tw-bg-onsgrey ons-u-p-xxs">
+  <span class="tw-p-y-0.5 tw-px-2 tw-bg-black tw-text-white tw-font-bold ons-u-ml-xs">ALPHA</span>
+</div>
+
+<div class="tw-flex tw-items-center tw-gap-3">
+  <div class="ons-u-p-xs">
+    <Logo />
+  </div>
+  <div class="tw-bg-onsred ons-u-p-xs ons-u-pl-s ons-u-pr-s ons-u-fs-s">Census Atlas</div>
 </div>

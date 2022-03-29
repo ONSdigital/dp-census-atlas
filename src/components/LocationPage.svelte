88eaa683574f8e6e8765f9b49321ab3ae11cd85b
Valentina <vmilitaru28@gmail.com>
2022-03-29 10:47:30 +0100

    adds the logic to retrieve a households and people total value for a given gegraphy on LocationPage

--- src/components/LocationPage.svelte
+++ src/components/LocationPage.svelte
@@ -1,19 +1,40 @@
 <script lang="ts">
   import { _ } from "svelte-i18n";
-  import { page } from "$app/stores";
   import Heading from "./Heading.svelte";
   import LocationOverview from "./LocationOverview.svelte";
   import { selectedGeographyStore } from "../stores/stores";
+  import { fetchSelectedGeographyData, parseSelectedGeographyData } from "../data/fetchVizData";
+  import { getCodesForCategory } from "../helpers/categoryHelpers";
+
+  let householdsCodes = getCodesForCategory("housing", "size-of-household", "default", "1-person-households");
+  let peopleCodes = getCodesForCategory("health", "general-health", "default", "very-good");
+  let allHouseholdsTotal = "";
+  let allPeopleTotal = "";
 
   $: selectedGeographyDisplayName = $selectedGeographyStore?.displayName;
-  $: search = $page.url.search;
+
+  $: if ($selectedGeographyStore.geoCode) {
+    fetchSelectedGeographyData({
+      totalCode: householdsCodes.totalCode,
+      categoryCodes: householdsCodes.categoryCodes,
+      geoCode: $selectedGeographyStore.geoCode,
+    }).then((table) => {
+      allHouseholdsTotal = parseSelectedGeographyData(table, householdsCodes.totalCode).selectedGeographyTotal;
+    });
+    fetchSelectedGeographyData({
+      totalCode: peopleCodes.totalCode,
+      categoryCodes: peopleCodes.categoryCodes,
+      geoCode: $selectedGeographyStore.geoCode,
+    }).then((table) => {
+      allPeopleTotal = parseSelectedGeographyData(table, peopleCodes.totalCode).selectedGeographyTotal;
+    });
+  }
 </script>
 
 <Heading serviceTitle={selectedGeographyDisplayName ? selectedGeographyDisplayName : "England and Wales"} />
 <LocationOverview
-  {search}
-  allPeopleTotal="test1"
-  allHouseholdsTotal="test2"
+  {allPeopleTotal}
+  {allHouseholdsTotal}
   title="{$_('locationOverview.title')} {selectedGeographyDisplayName
     ? selectedGeographyDisplayName
     : 'England and Wales'}"

042e75a635a8d70975c02f05e5a0c31cad9891d3
Patrick Fleming <patrickjosephfleming@gmail.com>
2022-03-29 10:26:32 +0100

    completes styling

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,9 +6,13 @@
   <span class="tw-p-y-0.5 tw-px-2 tw-bg-black tw-text-white tw-font-bold ons-u-ml-xs">ALPHA</span>
 </div>
 
-<div class="tw-flex tw-items-center tw-gap-3">
-  <div class="ons-u-p-xs">
-    <Logo />
+<div class="xl:tw-max-w-[27rem]">
+  <div class="tw-flex tw-grow tw-shrink tw-basis-auto">
+    <div class="ons-u-p-xs ons-u-pl-s ons-u-pr-s">
+      <Logo />
+    </div>
+    <div class="tw-bg-onsred tw-grow-[2] tw-shrink tw-basis-auto ons-u-p-xs ons-u-pl-s ons-u-pr-s ons-u-fs-s">
+      Census Atlas
+    </div>
   </div>
-  <div class="tw-bg-onsred ons-u-p-xs ons-u-pl-s ons-u-pr-s ons-u-fs-s">Census Atlas</div>
 </div>

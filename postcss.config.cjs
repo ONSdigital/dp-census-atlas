938cb30b6390d02bd8e18bbe2a5e36c8526b1199
Michał Ordon <michal@tehidea.com>
2022-03-30 16:47:43 +0100

    Add PurgeCSS

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,5 +1,6 @@
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
+const purgecss = require("@fullhuman/postcss-purgecss");
 const cssnano = require("cssnano");
 
 const mode = process.env.NODE_ENV;
@@ -9,6 +10,9 @@ module.exports = {
   plugins: [
     tailwindcss,
     autoprefixer,
+    purgecss({
+      content: ["./src/**/*.{html,js,svelte,ts}"],
+    }),
     !dev &&
       cssnano({
         preset: "default",

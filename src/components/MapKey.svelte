f1eb0dadf6f740636a58a28b4c35d080140f102b
Patrick Fleming <patrickjosephfleming@gmail.com>
2022-03-31 11:37:48 +0100

    partially styles expanded component

--- src/components/MapKey.svelte
+++ src/components/MapKey.svelte
@@ -8,18 +8,27 @@
 {#if $vizStore}
   <div class={`tw-flex tw-justify-center tw-absolute tw-bottom-8 tw-left-1/2 -tw-translate-x-1/2`}>
     <div
-      class="tw-z-abovemap tw-bg-white tw-px-8 tw-pb-2.5 tw-pt-3.5 tw-border-[1px] tw-border-slate-500 tw-flex tw-flex-col tw-gap-1.5 tw-items-center"
+      class="tw-z-abovemap tw-bg-white tw-px-8 tw-pb-2.5 tw-pt-3.5 tw-border-[1px] tw-border-slate-500 tw-flex
+    tw-flex-col tw-items-center ons-u-fs-s"
     >
-      <div class="tw-text-center">
+      <div class="tw-text-center tw-text-[16px]">
         {$vizStore.params.category.name}
       </div>
-      <div class="tw-text-sm tw-mr-1">High</div>
-      {#each choroplethColours.reverse() as colour, i}
-        <div style={`background-color: ${colour};`}>
+      <div
+        class="tw-text-onswhite tw-w-28 tw-p-1 tw-pl-1.5 tw-mb-1"
+        style={`background-color: ${choroplethColours[4]};`}
+      >
+        High
+      </div>
+      {#each choroplethColours.slice().reverse() as colour, i}
+        <div
+          class={i < 2 ? "tw-text-onswhite tw-w-28 tw-p-1.5" : "tw-w-28 tw-p-1.5"}
+          style={`background-color: ${colour};`}
+        >
           {buckets[i]}
         </div>
       {/each}
-      <div class="tw-text-sm tw-ml-1">Low</div>
+      <div class="tw-w-28 tw-p-1 tw-pl-1.5 tw-mt-1" style={`background-color: ${choroplethColours[0]};`}>Low</div>
     </div>
   </div>
 {/if}

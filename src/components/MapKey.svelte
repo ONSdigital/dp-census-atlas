6f6ce97fcadf8a0318c18a9c56fbf6c9820f1ba6
Valentina <vmilitaru28@gmail.com>
2022-04-01 09:28:26 +0100

    styles the default view of the MapKey component on desktop

--- src/components/MapKey.svelte
+++ src/components/MapKey.svelte
@@ -3,18 +3,18 @@
   import { choroplethColours } from "../helpers/choroplethHelpers";
   import { calculateDataBreakBuckets } from "../helpers/mapKeyHelper";
   import { areAllDefined } from "../util/genUtil";
-  let buckets;
+  let buckets: string[];
   $: areAllDefined([$vizStore?.breaks]) &&
     (buckets = calculateDataBreakBuckets($vizStore.breaks, $vizStore?.minMaxVals[0]));
 </script>
 
 {#if $vizStore}
-  <div class={`tw-flex tw-justify-center tw-absolute tw-bottom-8 tw-left-1/2 -tw-translate-x-1/2`}>
+  <div class={`tw-flex tw-justify-center tw-absolute tw-right-0 tw-top-20`}>
     <div
-      class="tw-z-abovemap tw-bg-white tw-px-8 tw-pb-2.5 tw-pt-3.5 tw-border-[1px] tw-border-slate-500 tw-flex
+      class="tw-z-abovemap tw-bg-white tw-px-3 tw-pb-10 tw-pt-3.5 tw-flex
     tw-flex-col tw-items-center ons-u-fs-s"
     >
-      <div class="tw-text-center tw-text-[16px]">
+      <div class="tw-text-center tw-mb-2 tw-w-28 tw-text-[16px]">
         {$vizStore.params.category.name}
       </div>
       <div
@@ -24,14 +24,11 @@
         High
       </div>
       {#each choroplethColours.slice().reverse() as colour, i}
-        <div
-          class={i < 2 ? "tw-text-onswhite tw-w-28 tw-p-1.5" : "tw-w-28 tw-p-1.5"}
-          style={`background-color: ${colour};`}
-        >
+        <div class="tw-w-28 tw-p-1.5 {i < 2 ? 'tw-text-onswhite' : null}" style={`background-color: ${colour};`}>
           {buckets[i]}
         </div>
       {/each}
-      <div class="tw-w-28 tw-p-1 tw-pl-1.5 tw-mt-1" style={`background-color: ${choroplethColours[0]};`}>Low</div>
+      <div class="tw-w-28 tw-p-1  tw-pl-1.5 tw-mt-1" style={`background-color: ${choroplethColours[0]};`}>Low</div>
     </div>
   </div>
 {/if}

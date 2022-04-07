44de28d9fa5dee3a6e2a39b325d97bbb5cbebc22
petem-methods <pete.montgomery@methods.co.uk>
2022-04-07 16:46:28 +0100

    DRY up initLayers

--- src/map/layers.ts
+++ src/map/layers.ts
@@ -1,16 +1,23 @@
+import type { GeoType } from "../types";
+
 export const layers = [
   {
-    name: "lad",
+    name: "lad" as GeoType,
     urlTemplate: "https://cdn.jsdelivr.net/gh/VivianAllen/maptiles/authorities-2011/v4/{z}/{x}/{y}.pbf",
     minZoom: 5,
-    minZoomForOutlines: 8,
     idProperty: "lad11cd",
   },
   {
-    name: "msoa",
+    name: "msoa" as GeoType,
     urlTemplate: "https://cdn.ons.gov.uk/maptiles/administrative/msoa/v2/boundaries/{z}/{x}/{y}.pbf",
     minZoom: 9,
-    minZoomForOutlines: 13,
     idProperty: "areacd",
   },
 ];
+
+export const layersWithSiblings = () => {
+  return layers
+    .slice(1)
+    .map((l, i) => ({ layer: layers[i], next: l }))
+    .concat({ layer: layers[layers.length - 1], next: undefined });
+};

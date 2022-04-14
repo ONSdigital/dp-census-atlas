b8a3bb139eb600546021fda14ac55ee53aaac0e9
Vivian Allen <vivian.allen@methods.co.uk>
2022-04-14 12:09:57 +0100

    add develop origin for serving from develop.onsdigital.co.uk/census-atlas
    Also remove census-atlas test page

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,6 +2,7 @@
 
 pushd dp-census-atlas
   npm install --silent
+  export ORIGIN="https://develop.onsdigital.co.uk/census-atlas"
   make build
   cp -r build package.json Dockerfile.concourse ../build
 popd

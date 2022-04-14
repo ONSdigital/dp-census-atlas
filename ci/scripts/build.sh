ac9cdb925202f6fe6a740b65fa8d3554f2edc915
Vivian Allen <vivian.allen@methods.co.uk>
2022-04-14 12:49:29 +0100

    Revert "Merge branch 'feature/develop-origin-for-node-build' into develop"
    This reverts commit fa10e2e2f41a36dd6b5b1f54bc14360f2fdc0e26, reversing
    changes made to 62eaad04e365b3f5282c821c28a27e37bc9da51a.

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,7 +2,6 @@
 
 pushd dp-census-atlas
   npm install --silent
-  export ORIGIN="https://develop.onsdigital.co.uk/census-atlas"
   make build
   cp -r build package.json Dockerfile.concourse ../build
 popd

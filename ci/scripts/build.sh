bb4053ff6673ed8eb5556356a3ada755ac2a7b29
Vivian Allen <vivian.allen@methods.co.uk>
2022-04-29 14:25:38 +0100

    scrape git branch from git log in CI build tasks

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,7 +1,23 @@
 #!/bin/bash -eux
 
 pushd dp-census-atlas
-  git show-ref | grep $(git log --pretty=%h -1) | sed 's|.*/\(.*\)|\1|' | sort -u | grep -v HEAD
+
+  GIT_BRANCH=$(git show-ref | grep $(git log --pretty=%h -1) | sed 's|.*/\(.*\)|\1|' | sort -u | grep -v HEAD)
+  case $GIT_BRANCH in
+
+    "develop")
+      echo "Building for develop"
+      ;;
+
+    "master")
+      echo "Building for production"
+      ;;
+
+    *)
+      echo "Neither master or develop branch found"
+      ;;
+  esac
+
   npm install --silent
   make build
   cp -r build package.json Dockerfile.concourse ../build

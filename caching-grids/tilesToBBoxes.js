7ac526212f6114fc81fab89bef593197818a9b59
Vivian Allen <vivian.allen@methods.co.uk>
2022-05-04 19:58:35 +0100

    add caching-grids/tilesToBBoxes.js, converts tile grid files to long,lat bboxes

---
+++ caching-grids/tilesToBBoxes.js
@@ -0,0 +1,19 @@
+import { tileToBBOX } from "@mapbox/tilebelt";
+import { readFileSync, writeFileSync } from "fs";
+
+if (process.argv.length < 3) {
+  throw "Please supply a JSON file of tiles to convert.";
+}
+
+const tileJSONfn = process.argv[2];
+const quadTiles = JSON.parse(readFileSync(tileJSONfn));
+const quadBBoxes = {};
+
+for (const key in quadTiles) {
+  quadBBoxes[key] = quadTiles[key].map((tile) => {
+    return tileToBBOX(tile);
+  });
+}
+
+const tileJSONfnNoEx = tileJSONfn.split(".")[0];
+writeFileSync(`${tileJSONfnNoEx}BBoxes.json`, JSON.stringify(quadBBoxes));

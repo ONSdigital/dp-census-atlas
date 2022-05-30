70b599ca8835048dad823b95773431d89937d8a8
Vivian Allen <vivian.allen@methods.co.uk>
2022-05-30 20:14:05 +0100

    simple logging of incoming health checks

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -2,6 +2,7 @@ import type { RequestHandler } from "@sveltejs/kit";
 
 // simple healthcheck endpoint for use by ONS CI as heartbeat
 export const get: RequestHandler = async ({ params }) => {
+  console.log(`Health check received: ${params}`);
   return {
     status: 200,
     body: {

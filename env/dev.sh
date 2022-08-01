70805613c215b04ebb758cb1a57c82d70685d4e3
Vivian Allen <vivian.allen@methods.co.uk>
2022-08-01 11:47:38 +0100

    reference new format 2011 content.json in dev.sh

--- env/dev.sh
+++ env/dev.sh
@@ -3,12 +3,6 @@
 export SKADAPTER=""
 export VITE_GEODATA_BASE_URL="https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/quads"
 export VITE_CONTENT_JSONS='[
-    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/population-content.json",
-    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/education-content.json",
-    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/housing-content.json",
-    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/work-content.json",
-    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/health-content-pt1.json",
-    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/health-content-pt2.json",
-    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/identity-content.json"
+    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2011-new-format-content.json"
 ]'
 export VITE_APP_BASE_PATH=""

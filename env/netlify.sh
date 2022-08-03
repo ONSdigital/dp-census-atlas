216c86e90deb94059c6b66ba8291d9f9795e27c7
Vivian Allen <vivian.allen@methods.co.uk>
2022-08-03 08:39:40 +0100

    add local fileserver for local-only content
    - add http-server script, serve-local-content-jsons, to package.json.
    Script sets up local file server on localhost:8090 to serve anything
    found in content/content_jsons. This change needed to test new or
    sensitive content locally without needing to host it on s3 / the
    ONS files API, etc.
    
    - refactor env/dev.sh to allow override of hardcoded env var values
    for VITE_CONTENT_JSONS and VITE_GEODATA_BASE_URL with env vars set
    when `npm run dev` is invoked. This change needed to allow use of
    the local fileserver for content_json files, as otherwise the
    hardcoded content_json / data urls would be used.
    
    - add env/netlify.sh with env var config for netlify builds. This
    change needed as with the above overrides, the dev env and the
    netlify env require different config.

--- env/netlify.sh
+++ env/netlify.sh
@@ -1,19 +1,8 @@
-# env vars needed to run locally
+# env vars needed to run on netlify
 
 export SKADAPTER=""
-# allow override of these values with locally set ones
-if [ -z $VITE_GEODATA_BASE_URL]; then
 export VITE_GEODATA_BASE_URL="https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/quads"
-else
-export VITE_GEODATA_BASE_URL=$VITE_GEODATA_BASE_URL
-fi
-
-if [ -z $VITE_CONTENT_JSONS]; then
 export VITE_CONTENT_JSONS='[
     "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2011-new-format-content.json"
 ]'
-else
-export VITE_CONTENT_JSONS=$VITE_CONTENT_JSONS
-fi
-
 export VITE_APP_BASE_PATH=""

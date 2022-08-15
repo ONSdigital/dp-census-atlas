19468d8f14dd54111ef8fe667244fc4eee094f72
Vivian Allen <vivian.allen@methods.co.uk>
2022-08-15 16:07:40 +0100

    fix overridable env var exports in dev.sh
    The boolean checks for already-set env vars in dev.sh (intended to
    allow easy config changes when running the dev app) didn't work due
    to bash syntax errors.

--- env/dev.sh
+++ env/dev.sh
@@ -2,13 +2,13 @@
 
 export SKADAPTER=""
 # allow override of these values with locally set ones
-if [ -z $VITE_GEODATA_BASE_URL]; then
+if [ -z ${VITE_GEODATA_BASE_URL+x} ]; then
 export VITE_GEODATA_BASE_URL="https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/quads"
 else
 export VITE_GEODATA_BASE_URL=$VITE_GEODATA_BASE_URL
 fi
 
-if [ -z $VITE_CONTENT_JSONS]; then
+if [ -z ${VITE_CONTENT_JSONS+x} ]; then
 export VITE_CONTENT_JSONS='[
     "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2011-new-format-content.json"
 ]'

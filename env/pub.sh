9b0195bb8db2472809306dc8ff906ff53aa5164d
Vivian Allen <vivian.allen@methods.co.uk>
2022-08-16 15:04:48 +0100

    add pub env

---
+++ env/pub.sh
@@ -0,0 +1,10 @@
+# env vars needed to run on netlify
+
+export SKADAPTER=""
+export VITE_GEODATA_BASE_URL="https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/quads"
+export VITE_CONTENT_JSONS='[
+    "https://localhost:28100/files-proxy/Education/2011-Education-new-format-content.json"
+]'
+export VITE_APP_BASE_PATH=""
+export VITE_IS_PUBLISHING="true"
+export VITE_PUBLISHING_DOWNLOAD_URL="http://localhost:23201/v1/downloads-new"

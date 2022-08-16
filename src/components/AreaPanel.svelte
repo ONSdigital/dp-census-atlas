f73c520cd8a859b7363847116fa76011762c7de9
petem-methods <pete.montgomery@methods.co.uk>
2022-08-16 14:41:51 +0100

    Wip

--- src/components/AreaPanel.svelte
+++ src/components/AreaPanel.svelte
@@ -1,23 +1,21 @@
 <script lang="ts">
-  import { englandAndWales } from "../helpers/spatialHelper";
   import { selectedGeographyStore } from "../stores/stores";
+  import GeoSearch from "./GeoSearch.svelte";
   import Icon from "./MaterialIcon.svelte";
   import { deselectGeography } from "../helpers/geographyHelper";
 
   $: selectedGeographyDisplayName = $selectedGeographyStore?.displayName;
   $: selectedGeographyGeoType = $selectedGeographyStore?.geoType.toUpperCase();
 
-  const resetSelectedGeography = () => {
-    deselectGeography();
-  };
-
-  let q = "";
-
   $: open = false;
   const toggleOpen = () => {
     open = !open;
   };
   $: openOrCloseAreaSearchPanelText = open ? "Close area search panel" : "Open area search panel";
+
+  const resetSelectedGeography = () => {
+    deselectGeography();
+  };
 </script>
 
 <section class="py-3 border-b-2">
@@ -55,33 +53,7 @@
 
   {#if open}
     <div class="mt-3 mb-1">
-      <div class="flex max-w-[25rem]">
-        <input
-          id="area-input"
-          name="area-input"
-          type="search"
-          autocomplete="off"
-          class="flex items-center justify-center h-12 p-2 w-full border-l-2 border-t-2 border-b-2 border-black focus:border-4 custom-ring"
-          bind:value={q}
-        />
-        <button tabindex="-1" type="submit" class="bg-onsblue px-3">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 text-white "
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="3"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
-      </div>
-      <div class="mt-2 text-sm text-onsdark">For example, your home town, a postcode or district</div>
+      <GeoSearch />
     </div>
   {/if}
 </section>
---
+++ src/components/GeoSearch.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  let q = "";
+
+  import { writable } from "svelte/store";
+
+  let tick = writable(0, () => {
+    let interval = setInterval(() => {
+      tick.update((value) => value + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  });
+
+  let tickValue = 0;
+  tick.subscribe((v) => {
+    tickValue = v;
+  });
+</script>
+
+<div class="">
+  Tick: {tick}
+</div>
+<div class="flex max-w-[25rem]">
+  <input
+    id="area-input"
+    name="area-input"
+    type="search"
+    autocomplete="off"
+    class="flex items-center justify-center h-12 p-2 w-full border-l-2 border-t-2 border-b-2 border-black focus:border-4 custom-ring"
+    bind:value={q}
+  />
+
+  <button tabindex="-1" type="submit" class="bg-onsblue px-3">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 text-white "
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="3"
+        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+      />
+    </svg>
+  </button>
+</div>
+<div class="mt-2 text-sm text-onsdark">For example, your home town, a postcode or district</div>

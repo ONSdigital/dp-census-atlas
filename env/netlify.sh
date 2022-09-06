1a250957694c71fdf0f9d92919db922a46e5f827
Vivian Allen <vivian.allen@methods.co.uk>
2022-09-06 11:41:14 +0100

    TEMP COMMIT point netlify at publishable bucket

--- env/netlify.sh
+++ env/netlify.sh
@@ -1,8 +1,8 @@
 # env vars needed to run on netlify
 
 export SKADAPTER=""
-export VITE_GEODATA_BASE_URL="https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads"
+export VITE_GEODATA_BASE_URL="https://ons-dp-sandbox-publishable-bucket-2.s3.eu-west-2.amazonaws.com/newquads/"
 export VITE_CONTENT_JSONS='[
-    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2011-TEST-PLACEHOLDER-VAR-DESCS-all-atlas-content-2022-09-02-11-58-45.json"
+    "https://ons-dp-sandbox-publishable-bucket-2.s3.eu-west-2.amazonaws.com/newquads/2011-TEST-PLACEHOLDER-VAR-DESCS-all-atlas-content-2022-09-02-11-58-45.json"
 ]'
 export VITE_APP_BASE_PATH=""

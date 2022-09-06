8bbc0406579440050aab347ec5294b14e44ba5c4
Vivian Allen <vivian.allen@methods.co.uk>
2022-09-06 11:55:44 +0100

    fix trailing slash in netlify geodata base url

--- env/netlify.sh
+++ env/netlify.sh
@@ -1,7 +1,7 @@
 # env vars needed to run on netlify
 
 export SKADAPTER=""
-export VITE_GEODATA_BASE_URL="https://ons-dp-sandbox-publishable-bucket-2.s3.eu-west-2.amazonaws.com/newquads/"
+export VITE_GEODATA_BASE_URL="https://ons-dp-sandbox-publishable-bucket-2.s3.eu-west-2.amazonaws.com/newquads"
 export VITE_CONTENT_JSONS='[
     "https://ons-dp-sandbox-publishable-bucket-2.s3.eu-west-2.amazonaws.com/newquads/2011-TEST-PLACEHOLDER-VAR-DESCS-all-atlas-content-2022-09-02-11-58-45.json"
 ]'

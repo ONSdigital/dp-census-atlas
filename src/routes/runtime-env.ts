4c0c1259e57f7d9d21e78813835d9f43d1a7c4f2
Vivian Allen <vivian.allen@methods.co.uk>
2022-09-07 18:36:46 +0100

    fix runtime-env ep calls from setContentStores on census-atlas basepath
    - Fix bug where setContentStores calls to runtime-env endpoint where
    not accounting for app base paths other than "".
    
    - Remove reundant params arg from runtime-env endpoint request
    handler func.

--- src/routes/runtime-env.ts
+++ src/routes/runtime-env.ts
@@ -1,7 +1,7 @@
 import type { RequestHandler } from "@sveltejs/kit";
 
 // return values from env set in runtime env (default to values set at build time if runtime env is missing var)
-export const get: RequestHandler = async ({ params }) => {
+export const get: RequestHandler = async () => {
   return {
     status: 200,
     body: {

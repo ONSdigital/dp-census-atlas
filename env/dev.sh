aacce2b1909b1694b9bacb79084ee1e6a1401289
Vivian Allen <vivian.allen@methods.co.uk>
2022-09-08 11:50:28 +0100

    update env config to use 2011-with-topic-groups.json

--- env/dev.sh
+++ env/dev.sh
@@ -1,19 +1,8 @@
 # env vars needed to run locally
 
 export SKADAPTER=""
-# allow override of these values with locally set ones
-if [ -z $VITE_GEODATA_BASE_URL]; then
 export VITE_GEODATA_BASE_URL="https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads"
-else
-export VITE_GEODATA_BASE_URL=$VITE_GEODATA_BASE_URL
-fi
-
-if [ -z $VITE_CONTENT_JSONS]; then
 export VITE_CONTENT_JSONS='[
-    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2011-TEST-PLACEHOLDER-VAR-DESCS-all-atlas-content-2022-09-02-11-58-45.json"
+    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2011-with-topic-groups.json"
 ]'
-else
-export VITE_CONTENT_JSONS=$VITE_CONTENT_JSONS
-fi
-
 export VITE_APP_BASE_PATH=""
--- env/ons.sh
+++ env/ons.sh
@@ -3,6 +3,6 @@
 export SKADAPTER="node"
 export VITE_GEODATA_BASE_URL="https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads"
 export VITE_CONTENT_JSONS='[
-    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2011-TEST-PLACEHOLDER-VAR-DESCS-all-atlas-content-2022-09-02-11-58-45.json"
+    "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2011-with-topic-groups.json"
 ]'
 export VITE_APP_BASE_PATH="/census-atlas"

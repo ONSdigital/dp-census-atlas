be21147f373d0b9d28016ff59b0a2229cb54e583
Vivian Allen <vivian.allen@methods.co.uk>
2022-09-14 08:47:35 +0100

    update all envs to use new content.json format

--- src/data/content.ts
+++ src/data/content.ts
@@ -10,13 +10,15 @@ export default {
   ],
   netlify: [
     {
-      contentJsonUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads/2011-all-topics.json",
+      contentJsonUrl:
+        "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads/2011-all-topics-with-variable-groups.json",
       contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads",
     },
   ],
   sandbox: [
     {
-      contentJsonUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads/2011-all-topics.json",
+      contentJsonUrl:
+        "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads/2011-all-topics-with-variable-groups.json",
       contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads",
     },
   ],

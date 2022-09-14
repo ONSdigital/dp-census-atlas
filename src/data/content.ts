85b807f986b53a97c7a202b51db43dac915530bd
Vivian Allen <vivian.allen@methods.co.uk>
2022-09-14 12:03:52 +0100

    TEMP point netlify at test content file for new short variable descs

--- src/data/content.ts
+++ src/data/content.ts
@@ -11,7 +11,7 @@ export default {
   netlify: [
     {
       contentJsonUrl:
-        "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads/2011-all-topics-with-variable-groups.json",
+        "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2011-TEST-SHORT-VAR-DESC-TEST-all-atlas-content-2022-09-14-12-34-37.json",
       contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/newquads",
     },
   ],

03338336919b94fcaeb56f26792b00e533285658
Vivian Allen <vivian.allen@methods.co.uk>
2022-10-04 13:40:49 +0100

    add 2021 geoLookup json for search

--- src/routes/api/geo/+server.ts
+++ src/routes/api/geo/+server.ts
@@ -1,12 +1,12 @@
 import { json, type RequestHandler } from "@sveltejs/kit";
-import data from "../../../data/geoLookup.json";
+import data from "../../../data/geoLookup2021.json";
 
 export const GET: RequestHandler = ({ url }) => {
   const q = url.searchParams.get("q").toLowerCase();
 
   if (q) {
     const results = data.filter(
-      (geo) => geo.en.toLowerCase().includes(q) || geo.cy.toLowerCase().includes(q) || geo.geoCode.toLowerCase() === q,
+      (geo) => geo.en.toLowerCase().includes(q) || geo.geoCode.toLowerCase() === q,
     );
     return json(results);
   } else {

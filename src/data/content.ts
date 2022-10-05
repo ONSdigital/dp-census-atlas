7481c838e75708cccf4b7efad6d3ed1ed1ff2ac7
Vivian Allen <vivian.allen@methods.co.uk>
2022-10-05 15:39:07 +0100

    update dev/netlify/sandbox content.json to new 2021-v1-ALL.json

--- src/data/content.ts
+++ src/data/content.ts
@@ -4,21 +4,21 @@ export default {
   dev: [
     {
       contentJsonUrl:
-        "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2021-all-atlas-content-incomplete-short-var-descs.json",
+        "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2021-v1-ALL.json",
       contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
     },
   ],
   netlify: [
     {
       contentJsonUrl:
-        "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2021-all-atlas-content-incomplete-short-var-descs.json",
+        "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2021-v1-ALL.json",
       contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
     },
   ],
   sandbox: [
     {
       contentJsonUrl:
-        "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2021-all-atlas-content-incomplete-short-var-descs.json",
+        "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/content-json/2021-v1-ALL.json",
       contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
     },
   ],

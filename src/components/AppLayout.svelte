cc118a4be4b5e655de90593e12cc5937fceab67e
petem-methods <pete.montgomery@methods.co.uk>
2022-10-10 20:37:42 +0100

    Stick the Use and share section to bottom

--- src/components/AppLayout.svelte
+++ src/components/AppLayout.svelte
@@ -14,7 +14,10 @@
     <Header />
   {/if}
   <div class="flex-1 flex flex-col-reverse lg:flex-row overflow-y-auto ">
-    <div class="flex-1 grow-[4] min-w-[30rem] max-w-[35rem] overflow-y-auto" class:hidden={$appParamsStore.embed}>
+    <div
+      class="flex-1 grow-[4] min-w-[30rem] max-w-[35rem] overflow-y-auto flex flex-col"
+      class:hidden={$appParamsStore.embed}
+    >
       <slot />
     </div>
     <div class="flex-1 grow-[7] relative">

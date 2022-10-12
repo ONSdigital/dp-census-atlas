38947e774dca3049723921ebb950a4e177eef6c1
Ahmad Barclay <abarclay.ons@gmail.com>
2022-10-12 12:05:02 +0100

    removed redundant queryRenderedFeatures call

--- src/map/renderMapViz.ts
+++ src/map/renderMapViz.ts
@@ -3,24 +3,17 @@ import type { VizData } from "../types";
 import { layers } from "./layers";
 
 export const renderMapViz = (map: mapboxgl.Map, data: VizData) => {
-  if (!data) {
+  if (!data?.places) {
     return;
   }
 
   const layer = layers.find((l) => l.name == data.geoType);
 
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore (typings for this overload are currently missing)
-  const features = map.queryRenderedFeatures({ layers: [`${layer.name}-features`] });
-
-  features.forEach((f) => {
-    const dataForFeature = data.places.find((p) => p.geoCode === f.id);
-    if (dataForFeature) {
-      map.setFeatureState(
-        { source: layer.name, sourceLayer: layer.sourceLayer, id: f.id },
-        { colour: getChoroplethColour(dataForFeature.ratioToTotal, data.breaks) },
-      );
-    }
+  data.places.forEach((p) => {
+    map.setFeatureState(
+      { source: layer.name, sourceLayer: layer.sourceLayer, id: p.geoCode },
+      { colour: getChoroplethColour(p.ratioToTotal, data.breaks) },
+    );
   });
 };
 

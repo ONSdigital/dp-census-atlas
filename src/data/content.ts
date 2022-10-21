745644dc1ddbc11a2d4dfd08881462ffcb2a9719
Vivian Allen <vivian.allen@methods.co.uk>
2022-10-21 08:23:33 +0100

    make locally-served content json default for dev

--- src/data/content.ts
+++ src/data/content.ts
@@ -51,6 +51,58 @@ const fakeCensus2021 = {
   },
 }
 
+const fakeCensus2021Local = {
+  zero: {
+    contentJsonUrl: "http://localhost:8090/release-0.json",
+    contentBaseUrl: "",
+  },
+  dem:  {
+    contentJsonUrl:
+      "http://localhost:8090/2021/2021-DEM.json",
+    contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
+  },
+  edu: {
+    contentJsonUrl:
+      "http://localhost:8090/2021/2021-EDU.json",
+    contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
+  },
+  eilr: {
+    contentJsonUrl:
+      "http://localhost:8090/2021/2021-EILR.json",
+    contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
+  },
+  hou: {
+    contentJsonUrl:
+      "http://localhost:8090/2021/2021-HOU.json",
+    contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
+  },
+  huc: {
+    contentJsonUrl:
+      "http://localhost:8090/2021/2021-HUC.json",
+    contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
+  },
+  lab: {
+    contentJsonUrl:
+      "http://localhost:8090/2021/2021-LAB.json",
+    contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
+  },
+  mig: {
+    contentJsonUrl:
+      "http://localhost:8090/2021/2021-MIG.json",
+    contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
+  },
+  sogi: {
+    contentJsonUrl:
+      "http://localhost:8090/2021/2021-SOGI.json",
+    contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
+  },
+  ttw: {
+    contentJsonUrl:
+      "http://localhost:8090/2021/2021-TTW.json",
+    contentBaseUrl: "https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/FAKE/2021",
+  },
+}
+
 const fakeCensus2021preview = {
   zero: {
     contentJsonUrl: "https://publishing.dp.aws.onsdigital.uk/visualisations/dvc691/release-0.json",
@@ -61,15 +113,15 @@ const fakeCensus2021preview = {
 export default {
   dev: {
     web: [
-      fakeCensus2021.dem,
-      fakeCensus2021.edu,
-      fakeCensus2021.eilr,
-      fakeCensus2021.hou,
-      fakeCensus2021.huc,
-      fakeCensus2021.lab,
-      fakeCensus2021.mig,
-      fakeCensus2021.sogi,
-      fakeCensus2021.ttw,
+      fakeCensus2021Local.dem,
+      fakeCensus2021Local.edu,
+      fakeCensus2021Local.eilr,
+      fakeCensus2021Local.hou,
+      fakeCensus2021Local.huc,
+      fakeCensus2021Local.lab,
+      fakeCensus2021Local.mig,
+      fakeCensus2021Local.sogi,
+      fakeCensus2021Local.ttw,
     ],
     publishing:[]
   },

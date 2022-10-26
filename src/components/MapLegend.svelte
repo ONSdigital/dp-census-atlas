89fa7cbdb429f37a473fb5bfdcd76625e3325248
petem-methods <pete.montgomery@methods.co.uk>
2022-10-26 23:17:32 +0100

    Complete legend hover

--- src/components/MapLegend.svelte
+++ src/components/MapLegend.svelte
@@ -9,9 +9,24 @@
   import BreaksChart from "./BreaksChart.svelte";
   import GeoTypeBadge from "./GeoTypeBadge.svelte";
 
-  $: categoryValueForSelectedGeography = $viz?.places.find((p) => p.geoCode === $geography?.geoCode)?.ratioToTotal;
-  $: categoryValueForHoveredGeography = $viz?.places.find((p) => p.geoCode === $hovered?.geoCode)?.ratioToTotal;
+  $: valueForSelectedGeography = $viz?.places.find((p) => p.geoCode === $geography?.geoCode)?.ratioToTotal;
+  $: valueForHoveredGeography = $viz?.places.find((p) => p.geoCode === $hovered?.geoCode)?.ratioToTotal;
   $: breaks = $viz ? [$viz?.minMaxVals[0], ...$viz.breaks] : undefined;
+
+  // use the hovered geo, otherwise the selected
+  $: active = valueForHoveredGeography
+    ? {
+        geoType: $hovered?.geoType,
+        geoCode: $hovered?.geoCode,
+        displayName: $hovered?.displayName,
+        value: valueForHoveredGeography,
+      }
+    : {
+        geoType: $geography?.geoType,
+        geoCode: $geography?.geoCode,
+        displayName: $geography?.displayName,
+        value: valueForSelectedGeography,
+      };
 
   const legendTextClass = "text-sm sm:text-base lg:text-lg xl:text-xl";
 </script>
@@ -21,26 +36,18 @@
 <!-- no category selected | show no legend at all  | just show the geography name  -->
 <!--    category selected | show EW legend, no %   | full legend, with percentage  -->
 
-{#if $viz?.params?.category && $geography?.geoType !== "ew"}
+{#if $viz?.params?.category}
   <div class={`absolute bottom-3 lg:bottom-8 flex w-full justify-center`}>
     <div
       class="z-abovemap w-full max-w-[50rem] mx-3 lg:mx-4 bg-white bg-opacity-90 px-3 lg:px-5 py-2 lg:py-3 border-[1px] lg:border-[1px] border-ons-grey-15"
     >
-      <!-- <div class="mb-3">
-        <div class="">
-          {categoryValueForHoveredGeography}
-        </div>
-        <div class="">
-          {JSON.stringify($hovered)}
-        </div>
-      </div> -->
-      {#if $viz?.params?.category && categoryValueForSelectedGeography}
+      {#if $viz?.params?.category && (valueForSelectedGeography || valueForHoveredGeography)}
         <!-- full legend -->
         <div class="flex gap-3 items-center">
           <div class="whitespace-nowrap">
-            <span class="text-4xl md:text-5xl font-bold">
-              {dataToRoundedString(categoryValueForSelectedGeography)}</span
-            ><span class="text-3xl md:text-4xl font-bold">{getVariableDataSuffix($viz.params.variable)}</span>
+            <span class="text-4xl md:text-5xl font-bold"> {dataToRoundedString(active.value)}</span><span
+              class="text-3xl md:text-4xl font-bold">{getVariableDataSuffix($viz.params.variable)}</span
+            >
           </div>
           <div class="flex-grow leading-[0px]">
             <div class="">
@@ -48,16 +55,16 @@
                 {formatTemplateString(
                   $viz.params.variable,
                   $viz.params.category,
-                  $geography.displayName,
+                  active.displayName,
                   $viz.params.category.legend_str_1,
                 )}
               </span>
-              <GeoTypeBadge geoType={$geography?.geoType} />
+              <GeoTypeBadge geoType={active.geoType} />
               <span class={legendTextClass}>
                 {formatTemplateString(
                   $viz.params.variable,
                   $viz.params.category,
-                  $geography.displayName,
+                  active.displayName,
                   $viz.params.category.legend_str_2,
                 )}
               </span>
@@ -66,7 +73,7 @@
               {formatTemplateString(
                 $viz.params.variable,
                 $viz.params.category,
-                $geography.displayName,
+                active.displayName,
                 $viz.params.category.legend_str_3,
               )}
             </div>
@@ -77,9 +84,9 @@
         <div class="">
           <div class="">
             <span class={legendTextClass}>
-              {$geography.displayName}
+              {active.displayName}
             </span>
-            <GeoTypeBadge geoType={$geography?.geoType} />
+            <GeoTypeBadge geoType={active.geoType} />
           </div>
           {#if $viz?.params?.category}
             <div class={`${legendTextClass} font-bold`}>
@@ -91,7 +98,7 @@
 
       {#if $viz?.params?.category && $viz?.breaks}
         <BreaksChart
-          selected={categoryValueForSelectedGeography}
+          selected={active.value}
           suffix={getVariableDataSuffix($viz.params.variable)}
           {breaks}
           colors={choroplethColours}

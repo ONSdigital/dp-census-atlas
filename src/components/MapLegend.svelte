b3129ff3e0cd9d33338adfc518a149fb42a3a5dc
petem-methods <pete.montgomery@methods.co.uk>
2022-10-26 11:31:20 +0100

    Fix race in legend - use result, not input

--- src/components/MapLegend.svelte
+++ src/components/MapLegend.svelte
@@ -1,6 +1,5 @@
 <script>
   import { viz } from "../stores/viz";
-  import { selection } from "../stores/selection";
   import { geography } from "../stores/geography";
   import { formatTemplateString } from "../helpers/categoryHelpers";
   import { choroplethColours } from "../helpers/choroplethHelpers";
@@ -9,7 +8,6 @@
   import BreaksChart from "./BreaksChart.svelte";
   import GeoTypeBadge from "./GeoTypeBadge.svelte";
 
-  // todo: what's this?
   $: categoryValueForSelectedGeography = $viz?.places.find((p) => p.geoCode === $geography?.geoCode)?.ratioToTotal;
   $: breaks = $viz ? [$viz?.minMaxVals[0], ...$viz.breaks] : undefined;
 </script>
@@ -19,45 +17,45 @@
 <!-- no category selected | show no legend at all  | just show the geography name  -->
 <!--    category selected | show EW legend, no %   | full legend, with percentage  -->
 
-{#if $selection.category || $geography?.geoType !== "ew"}
+{#if $viz?.params?.category || $geography?.geoType !== "ew"}
   <div class={`absolute bottom-3 lg:bottom-8 flex w-full justify-center`}>
     <div
       class="z-abovemap w-full max-w-[50rem] mx-3 lg:mx-4 bg-white bg-opacity-90 px-3 lg:px-5 py-2 lg:py-3 border-[1px] lg:border-[1px] border-ons-grey-15"
     >
-      {#if $selection.category && categoryValueForSelectedGeography}
+      {#if $viz?.params?.category && categoryValueForSelectedGeography}
         <!-- full legend -->
         <div class="flex gap-3 items-center">
           <div class="whitespace-nowrap">
             <span class="text-4xl md:text-5xl font-bold">
               {dataToRoundedString(categoryValueForSelectedGeography)}</span
-            ><span class="text-3xl md:text-4xl font-bold">{getVariableDataSuffix($selection.variable)}</span>
+            ><span class="text-3xl md:text-4xl font-bold">{getVariableDataSuffix($viz.params.variable)}</span>
           </div>
           <div class="flex-grow leading-[0px]">
             <div class="">
               <span class="text-xs sm:text-base md:text-xl">
                 {formatTemplateString(
-                  $selection.variable,
-                  $selection.category,
+                  $viz.params.variable,
+                  $viz.params.category,
                   $geography.displayName,
-                  $selection.category.legend_str_1,
+                  $viz.params.category.legend_str_1,
                 )}
               </span>
               <GeoTypeBadge geoType={$geography?.geoType} />
               <span class="text-xs sm:text-base md:text-xl">
                 {formatTemplateString(
-                  $selection.variable,
-                  $selection.category,
+                  $viz.params.variable,
+                  $viz.params.category,
                   $geography.displayName,
-                  $selection.category.legend_str_2,
+                  $viz.params.category.legend_str_2,
                 )}
               </span>
             </div>
             <div class="text-xs sm:text-base md:text-xl font-bold">
               {formatTemplateString(
-                $selection.variable,
-                $selection.category,
+                $viz.params.variable,
+                $viz.params.category,
                 $geography.displayName,
-                $selection.category.legend_str_3,
+                $viz.params.category.legend_str_3,
               )}
             </div>
           </div>
@@ -71,18 +69,18 @@
             </span>
             <GeoTypeBadge geoType={$geography?.geoType} />
           </div>
-          {#if $selection.category}
+          {#if $viz?.params?.category}
             <div class="text-xs sm:text-base md:text-xl font-bold">
-              {$selection.category.name}
+              {$viz.params.category.name}
             </div>
           {/if}
         </div>
       {/if}
 
-      {#if $selection.category && $viz}
+      {#if $viz?.params?.category && $viz?.breaks}
         <BreaksChart
           selected={categoryValueForSelectedGeography}
-          suffix={getVariableDataSuffix($selection.variable)}
+          suffix={getVariableDataSuffix($viz.params.variable)}
           {breaks}
           colors={choroplethColours}
         />

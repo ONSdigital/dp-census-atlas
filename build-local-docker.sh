0cd1e4245414e492c63f2b725ada1040b09d15ac
Vivian Allen <vivian.allen@methods.co.uk>
2022-10-26 19:51:12 +0100

    WIP add basic local docker version and helper scripts

---
+++ build-local-docker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t census-maps-local-test -f Dockerfile.local .
---
+++ run-local-docker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker run -p 28100:28100 --name census-maps-local -m 256m --cpus=0.03 census-maps-local-test

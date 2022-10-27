389799ab8cfaaefeda80cd6f2bc4bca86e574515
petem-methods <pete.montgomery@methods.co.uk>
2022-10-27 16:33:36 +0100

    Complete legend hover

--- src/components/GeoTypeBadge.svelte
+++ src/components/GeoTypeBadge.svelte
@@ -6,7 +6,7 @@
 
 {#if geoType && geoType !== "ew"}
   <div
-    class="inline-block align-middle text-xs md:text-sm bg-ons-census text-white font-bold px-1 rounded-sm mx-0 md:mx-1 -mt-0.5 lg:-mt-1"
+    class="inline-block align-middle text-xs md:text-sm bg-ons-census text-white font-bold px-1 rounded-sm mx-0 md:mx-[3px] -mt-0.5 lg:-mt-[5px]"
   >
     {geoType.toUpperCase()}
   </div>
--- src/components/MapLegend.svelte
+++ src/components/MapLegend.svelte
@@ -2,7 +2,7 @@
   import { viz } from "../stores/viz";
   import { geography } from "../stores/geography";
   import { hovered } from "../stores/hovered";
-  import { selected } from "../stores/selected";
+  // import { selected } from "../stores/selected";
   import { formatTemplateString } from "../helpers/categoryHelpers";
   import { choroplethColours } from "../helpers/choroplethHelpers";
   import { getVariableDataSuffix } from "../helpers/contentHelpers";
@@ -10,23 +10,37 @@
   import BreaksChart from "./BreaksChart.svelte";
   import GeoTypeBadge from "./GeoTypeBadge.svelte";
 
-  // $: valueForSelectedGeography = $viz?.places.find((p) => p.geoCode === $geography?.geoCode)?.ratioToTotal;
+  $: valueForSelectedGeography = $viz?.places.find((p) => p.geoCode === $geography?.geoCode)?.ratioToTotal;
   $: valueForHoveredGeography = $viz?.places.find((p) => p.geoCode === $hovered?.geoCode)?.ratioToTotal;
   $: breaks = $viz ? [$viz?.minMaxVals[0], ...$viz.breaks] : undefined;
 
-  // use the hovered geo, otherwise the selected
-  $: active = valueForHoveredGeography
+  // use the hovered geo, otherwise the selected, otherwise the loaded geography
+  $: active = $hovered
     ? {
         geoType: $hovered?.geoType,
         geoCode: $hovered?.geoCode,
         displayName: $hovered?.displayName,
         value: valueForHoveredGeography,
       }
+    : // : $selected
+    // ? {
+    //     geoType: $selected?.geoType,
+    //     geoCode: $selected?.geoCode,
+    //     displayName: $selected?.displayName,
+    //     value: $selected?.value,
+    //   }
+    valueForSelectedGeography
+    ? {
+        geoType: $geography?.geoType,
+        geoCode: $geography?.geoCode,
+        displayName: $geography?.displayName,
+        value: valueForSelectedGeography,
+      }
     : {
-        geoType: $selected?.geoType,
-        geoCode: $selected?.geoCode,
-        displayName: $selected?.displayName,
-        value: $selected?.value,
+        geoType: $geography?.geoType,
+        geoCode: $geography?.geoCode,
+        displayName: $geography?.displayName,
+        value: undefined,
       };
 
   const legendTextClass = "text-sm sm:text-base lg:text-lg xl:text-xl";
@@ -37,15 +51,12 @@
 <!-- no category selected | show no legend at all  | just show the geography name  -->
 <!--    category selected | show EW legend, no %   | full legend, with percentage  -->
 
-{#if $viz?.params?.category}
+{#if active.geoType !== "ew" && ($viz?.params?.category || active.geoCode)}
   <div class={`absolute bottom-3 lg:bottom-8 flex w-full justify-center`}>
     <div
       class="z-abovemap w-full max-w-[50rem] mx-3 lg:mx-4 bg-white bg-opacity-90 px-3 lg:px-5 py-2 lg:py-3 border-[1px] lg:border-[1px] border-ons-grey-15"
     >
-      <div class="">
-        {JSON.stringify($selected?.value)}
-      </div>
-      {#if $viz?.params?.category && ($selected?.value || valueForHoveredGeography)}
+      {#if $viz?.params?.category && active.value}
         <!-- full legend -->
         <div class="flex gap-3 items-center">
           <div class="whitespace-nowrap">

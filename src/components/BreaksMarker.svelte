3dc622ed4e5334f20585a1b6429d3a92b93e0a11
petem-methods <pete.montgomery@methods.co.uk>
2022-10-31 11:51:30 +0000

    Better ghost marker

--- src/components/BreaksMarker.svelte
+++ src/components/BreaksMarker.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  export let colour = "black";
+  export let ghost = false;
 </script>
 
-<svg class="h-7 w-7" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-  <polygon points="10,10 90,10 50,78" stroke={colour} stroke-width="19" stroke-linejoin="round" fill="white" />
+<svg class="h-7 w-7" class:opacity-50={ghost} viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+  <polygon points="10,10 90,10 50,78" stroke="black" stroke-width="19" stroke-linejoin="round" fill="white" />
 </svg>

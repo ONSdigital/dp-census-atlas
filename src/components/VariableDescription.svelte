79443c5dea94417cfb88fa023736c9150b615649
petem-methods <pete.montgomery@methods.co.uk>
2022-10-31 11:51:17 +0000

    Enlarge clickable area for long description

--- src/components/VariableDescription.svelte
+++ src/components/VariableDescription.svelte
@@ -3,22 +3,26 @@
 
   export let shortDescription: string;
   export let longDescription: string;
-  let showLongDescription = false;
+  let open = false;
+  const toggleOpen = () => {
+    open = !open;
+  };
 </script>
 
-<div class="flex items-end gap-1">
+<div class="flex items-end gap-1 group hoverable" on:click={toggleOpen}>
   <div class="flex-grow">
     {shortDescription}
   </div>
 
   <button
-    class="flex-none text-xl mb-0.5 hover:scale-110 duration-75 transition-all custom-ring"
-    on:click={() => (showLongDescription = !showLongDescription)}
+    class="flex-none text-xl mb-0.5 group-hover:scale-125 duration-75 transition-all custom-ring"
+    title="Show full description"
+    aria-label="Show full description"
   >
     <Icon kind="info" />
   </button>
 </div>
-{#if showLongDescription}
+{#if open}
   <div class="my-5 border-l-4 border-l-ons-grey-75 p-4 bg-ons-grey-5" style="white-space:pre-wrap">
     {longDescription}
   </div>

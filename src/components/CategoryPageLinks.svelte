738442eb04c3d108a5609a78e277d6c49466e5d8
petem-methods <pete.montgomery@methods.co.uk>
2022-11-01 20:58:57 +0000

    Restore temporary download link

--- src/components/CategoryPageLinks.svelte
+++ src/components/CategoryPageLinks.svelte
@@ -2,15 +2,15 @@
   import EmbedThis from "./EmbedThis.svelte";
   import Icon from "./MaterialIcon.svelte";
 
+  // href={`https://www.ons.gov.uk/datasets/${dataset}/editions/2021/versions/1`}
   export let dataset: string;
 </script>
 
 <section class="">
   <h2 class="mb-2 text-md font-semibold md:text-lg ">Use and share</h2>
   <div class="flex items-center gap-6">
-    <!-- could add attribute download="file-not-yet-available.cvs" -->
     <a
-      href={`https://www.ons.gov.uk/datasets/${dataset}/editions/2021/versions/1`}
+      href={`https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/articles/demographyandmigrationdatacontent/2022-11-02`}
       class="flex items-center gap-2 custom-ring hyperlink -ml-0.5"
       data-gtm-type="data-download"
     >

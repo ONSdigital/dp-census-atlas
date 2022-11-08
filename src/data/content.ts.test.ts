2da4ecde854dde7375a41f89b757a11369afa01e
Vivian Allen <vivian.allen@methods.co.uk>
2022-11-08 10:58:25 +0000

    add more content tests to ensure no unpublished data in prod

--- src/data/content.ts.test.ts
+++ src/data/content.ts.test.ts
@@ -1,11 +1,36 @@
 import content from "./content";
 
+const publishedOrPrepublishedTopics = ["release-0", "dem", "mig"];
+
 describe("content", () => {
+  test("prod web contains the expected content only", () => {
+    // !!! NB THIS TEST IS A SAFETY CATCH - UPDATE THE publishedTopics ARRAY AS NECCESSARY !!!
+    let allPublished = true;
+    publishedOrPrepublishedTopics.forEach((pt) => {
+      const matchesPublishedTopic = content.prod.web.filter((t) => t.contentJsonUrl.toLowerCase().includes(pt));
+      if (matchesPublishedTopic.length === 0) {
+        allPublished = false;
+      }
+    });
+    expect(allPublished).toEqual(true);
+  });
   test("prod web contains only published prod content", () => {
     const nonPublishedNonProdContent = content.prod.web.filter((t) => {
       !t.contentJsonUrl.includes("ons.gov.uk") && !t.contentBaseUrl.includes("dp-prod");
     });
     expect(nonPublishedNonProdContent).toEqual([]);
+  });
+  test("prod web does not contain any blanks", () => {
+    const blankContent = content.prod.web.filter((t) => {
+      t.contentJsonUrl === "" || t.contentBaseUrl === "";
+    });
+    expect(blankContent).toEqual([]);
+  });
+  test("prod publishing does not contain any blanks", () => {
+    const blankContent = content.prod.publishing.filter((t) => {
+      t.contentJsonUrl === "" || t.contentBaseUrl === "";
+    });
+    expect(blankContent).toEqual([]);
   });
   test("dev does not contain prepublished prod data", () => {
     const prodPrePublishedContent = [

4165c8951dbbe6fb4d59f73836a96e24cf280f6c
petem-methods <pete.montgomery@methods.co.uk>
2022-11-15 13:57:17 +0000

    Correct spelling error

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -3,6 +3,6 @@
 pushd dp-census-atlas
   npm install --silent
   # install additional playwright deps...
-  $(npm bin)/playwrite --install-deps
+  $(npm bin)/playwright --install-deps
   make component
 popd

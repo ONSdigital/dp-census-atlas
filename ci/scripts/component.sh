852a8ceefcb753b996e0de896bdf36e4da9f505c
petem-methods <pete.montgomery@methods.co.uk>
2022-11-15 13:47:32 +0000

    Use playwright --install-deps

--- ci/scripts/component.sh
+++ ci/scripts/component.sh
@@ -3,21 +3,6 @@
 pushd dp-census-atlas
   npm install --silent
   # install additional playwright deps...
-  apt-get update && apt-get install -y libnss3 \
-    libnspr4 \
-    libatk1.0-0 \
-    libatk-bridge2.0-0\
-    libcups2 \
-    libdrm2 \
-    libdbus-1-3 \
-    libatspi2.0-0 \
-    libxcomposite1 \
-    libxdamage1 \
-    libxfixes3 \
-    libxrandr2 \
-    libgbm1 \
-    libxkbcommon0 \
-    libasound2 \
-    libwayland-client0
+  $(npm bin)/playwrite --install-deps
   make component
 popd

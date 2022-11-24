bcef8984b6c50217ece46549c0ad129a706c5948
petem-methods <pete.montgomery@methods.co.uk>
2022-11-24 14:48:26 +0000

    Correct  caveat code

--- src/helpers/caveatHelper.ts
+++ src/helpers/caveatHelper.ts
@@ -11,7 +11,7 @@ export const getCaveat = (v: Variable): Caveat | undefined => {
       text: "There are pandemic-related quality considerations for this variable.",
       link: "https://www.ons.gov.uk/releases/labourmarketandtraveltoworkcensus2021inenglandandwales",
     };
-  } else if (v.code === "national_identity" || v.code === "national_identity_detailed") {
+  } else if (v.code === "national_identity_all" || v.code === "national_identity_detailed") {
     return {
       text: "There are quality considerations for this variable.",
       link: "https://www.ons.gov.uk/peoplepopulationandcommunity/culturalidentity/ethnicity/methodologies/ethnicgroupnationalidentitylanguageandreligionqualityinformationforcensus2021",

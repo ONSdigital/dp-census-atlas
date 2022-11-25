323613943514c4b84127e9de724a8f5b2605985b
petem-methods <pete.montgomery@methods.co.uk>
2022-11-25 12:03:25 +0000

    Try to fix concourse tests

--- e2e/layout.spec.ts
+++ e2e/layout.spec.ts
@@ -1,10 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("some test....", async ({ page }) => {
-  // expect five legend colors for aged-under-1-year
-  // await page.goto("/choropleth/population/age/resident-age-101a/aged-under-1-year");
-  // await expect(page.locator(legendColorsSelector)).toHaveCount(5);
-});
+// todo: this is how to do a small screen test...
 
 test.use({
   viewport: { width: 375, height: 667 },

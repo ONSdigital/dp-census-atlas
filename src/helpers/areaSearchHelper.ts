f6f0d526b59ef583af2a398726da966dd2c154a9
Vivian Allen <vivian.allen@methods.co.uk>
2022-11-25 12:22:31 +0000

    replace postcodes.io with flat file postcode lookups from s3
    refactor area search components to use postcode lookup json files
    from s3 instead of calling postcodes.io. This change needed to avoid
    potential GDPR issues with postcodes.io

--- src/helpers/areaSearchHelper.ts
+++ src/helpers/areaSearchHelper.ts
@@ -1,10 +1,5 @@
-import type { Coord } from "@turf/helpers";
 import { appBasePath } from "../buildEnv";
 import type { GeographySearchItem, PostcodeSearchItem } from "../types";
-import Pbf from "pbf";
-import vt from "@mapbox/vector-tile";
-import tb from "@mapbox/tilebelt";
-import inPolygon from "@turf/boolean-point-in-polygon";
 
 export const isSearchableQuery = (query: string): boolean => {
   return query.length >= 3;
@@ -31,40 +26,14 @@ const fetchGeographySearchItems = async (q: string): Promise<GeographySearchItem
 
 const fetchPostcodeSearchItems = async (q: string): Promise<PostcodeSearchItem[]> => {
   try {
-    const response = await fetch(`https://api.postcodes.io/postcodes/${q}/autocomplete`);
+    const pcdPrefix = q.toUpperCase().replace(/\s/g, "").slice(0, 4);
+    const response = await fetch(
+      `https://ons-dp-sandbox-atlas-data.s3.eu-west-2.amazonaws.com/postcodeLkup/${pcdPrefix}.json`,
+    );
     const json = await response.json();
-    return (json.result ?? []).map((postcode) => ({ kind: "Postcode", value: postcode }));
+    return json.map((postcode) => ({ kind: "Postcode", value: postcode.pcd, oa: postcode.oa }));
   } catch (err) {
     console.error(err);
     return [] as PostcodeSearchItem[];
   }
 };
-
-export async function getOAfromLngLat(lng, lat) {
-  const tile = tb.pointToTile(lng, lat, 12);
-  const url = `https://cdn.ons.gov.uk/maptiles/administrative/2021/oa/v2/boundaries/${tile[2]}/${tile[0]}/${tile[1]}.pbf`;
-  try {
-    const geojson = await getTileAsGeoJSON(url, tile);
-    const pt = { type: "Point", coordinates: [lng, lat] };
-    for (const f of geojson.features) {
-      if (inPolygon(pt as Coord, f.geometry)) return f.properties.areacd;
-    }
-    return null;
-  } catch {
-    return null;
-  }
-}
-
-async function getTileAsGeoJSON(url, tile) {
-  const res = await fetch(url);
-  const buf = await res.arrayBuffer();
-  const pbf = new Pbf(buf);
-  const geojson = { type: "FeatureCollection", features: [] };
-  const t = new vt.VectorTile(pbf);
-  for (const key in t.layers) {
-    for (let i = 0; i < t.layers[key].length; i++) {
-      geojson.features.push(t.layers[key].feature(i).toGeoJSON(...tile));
-    }
-  }
-  return geojson;
-}

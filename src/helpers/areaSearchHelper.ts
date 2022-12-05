4869db93850e148e243dcfdb43f5a2d27f6c6fbf
Vivian Allen <vivian.allen@methods.co.uk>
2022-12-05 09:39:26 +0000

    filterPostcodeResults prioritises matches with query as user typed it
    areaSearchHelper.filterPostcodeResults first filters to results
    that match the query as the user types it (case corrected), and only
    if there are no matches will it return results after removing spaces
    from the users query. This is to avoid issues where postcodes overlap
    onces spaces are removed, e.g. 'PO14 xxx' vs 'PO1 4xx'.

--- src/helpers/areaSearchHelper.ts
+++ src/helpers/areaSearchHelper.ts
@@ -41,8 +41,19 @@ const fetchPostcodeSearchItems = async (q: string): Promise<PostcodeSearchItem[]
   }
 };
 
+/*
+  Filter postcodes to first ten matches for query string. Prioritise matches with the query string as the user typed it
+  (after enforcing case) but if no matches found try to filter postcodes for the user query with spaces removed.
+*/
 const filterPostcodeResults = (q: string, postcodes: PostcodeSearchItem[]): PostcodeSearchItem[] => {
-  return postcodes
+  const matchesOriginalQuery = postcodes
+    .filter((pcd) => pcd.value.toUpperCase().includes(q.toUpperCase()))
+    .slice(0, maxPostcodeResults);
+  if (matchesOriginalQuery.length > 0) {
+    return matchesOriginalQuery;
+  }
+  const matchesWOSpaces = postcodes
     .filter((pcd) => pcd.value.toUpperCase().replace(/\s/g, "").includes(q.toUpperCase().replace(/\s/g, "")))
     .slice(0, maxPostcodeResults);
+  return matchesWOSpaces;
 };

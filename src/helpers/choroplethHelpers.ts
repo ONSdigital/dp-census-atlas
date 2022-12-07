4c0c2df661e6e4bd0631be4172e287a16acc0fdc
Vivian Allen <vivian.allen@methods.co.uk>
2022-12-07 14:43:24 +0000

    flip negative heatmap

--- src/helpers/choroplethHelpers.ts
+++ src/helpers/choroplethHelpers.ts
@@ -1,5 +1,5 @@
 export const choroplethColours = ["#CDE594", "#80C6A3", "#1F9EB7", "#186290", "#080C54"];
-export const negativeColours = ["#fff33b", "#fdc70c", "#f3903f", "#ed683c", "#e93e3a"];
+export const negativeColours = ["#e93e3a", "#ed683c", "#f3903f", "#fdc70c", "#fff33b"];
 
 export const getColoursForBreaks = (breaks: number[]): string[] => {
   if (breaks[0] > 0) {

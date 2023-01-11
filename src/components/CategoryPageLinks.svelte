f095afcad63f518c6a83fb38112ffaff94eb0336
petem-methods <pete.montgomery@methods.co.uk>
2023-01-11 16:38:05 +0000

    Add feedback link

--- src/components/CategoryPageLinks.svelte
+++ src/components/CategoryPageLinks.svelte
@@ -10,15 +10,18 @@
 
 <section class="">
   <h2 class="mb-2 text-md font-semibold md:text-lg ">Use and share</h2>
-  <div class="flex items-center gap-x-6 gap-y-2 flex-wrap ">
+  <div class="flex items-center gap-x-6 lg:gap-x-8 gap-y-2 flex-wrap ">
     <div>
       <Share />
     </div>
-    {#if !$params.embed}
-      <div class="hidden md:block">
-        <EmbedThis />
+
+    <a href="https://www.ons.gov.uk/feedback" class="flex items-center gap-2 custom-ring hyperlink -ml-0.5">
+      <div class="text-2xl text-ons-black">
+        <Icon kind="comment" />
       </div>
-    {/if}
+      <div>Give us feedback</div>
+    </a>
+
     {#if dataDownloadUrl}
       <a
         href={dataDownloadUrl}
@@ -30,6 +33,12 @@
         </div>
         <div>Download data</div>
       </a>
+    {/if}
+
+    {#if !$params.embed}
+      <div class="hidden md:block">
+        <EmbedThis />
+      </div>
     {/if}
   </div>
 </section>

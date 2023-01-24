342ee40b3b2f25883f6b206cfef25bfb736d4ce7
Vivian Allen <vivian.allen@methods.co.uk>
2023-01-24 10:14:46 +0000

    WIP noData color -> dark grey

--- src/helpers/choroplethHelpers.ts
+++ src/helpers/choroplethHelpers.ts
@@ -3,7 +3,7 @@ export const colours = {
   neg: ["#292567", "#50488E", "#7A6CB8", "#A495DC", "#BEB7D7"],
   pos: ["#FDCC88", "#FCA540", "#EA8223", "#D46012", "#BC3B00"],
   mid: "#DDDDDD",
-  noData: "#FF00FF",
+  noData: "#273746",
 };
 
 export const getColoursForBreaks = (breaks: number[], changeOverTime: boolean): string[] => {

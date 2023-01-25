3915794d4b9d06face968de0fb5a697b54fd3965
petem-methods <pete.montgomery@methods.co.uk>
2023-01-25 15:25:30 +0000

    Fix icon  vertical alignment

--- src/components/MapTips.svelte
+++ src/components/MapTips.svelte
@@ -7,7 +7,7 @@
   <div class="absolute top-[48%] left-1/2 -translate-x-1/2">
     <div class="px-3 py-1  rounded bg-ons-census-supporting text-ons-white">
       <div class="flex gap-2 items-center">
-        <div class="hidden lg:block text-2xl">
+        <div class="hidden lg:flex text-2xl">
           <Icon kind="keyboardBackspace" />
         </div>
         <div>Select a topic to visualise</div>

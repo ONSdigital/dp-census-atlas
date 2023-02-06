a24318b1ec1d9a3b3893d08a8bb20d181fca194f
Vivian Allen <vivian.allen@methods.co.uk>
2023-02-06 11:06:40 +0000

    new colorscheme: datawrapper

--- src/helpers/choroplethHelpers.ts
+++ src/helpers/choroplethHelpers.ts
@@ -1,8 +1,8 @@
 export const choroplethColours = {
   standard: ["#CDE594", "#80C6A3", "#1F9EB7", "#186290", "#080C54"],
-  neg: ["#292567", "#50488E", "#7A6CB8", "#A495DC", "#BEB7D7"],
-  pos: ["#FDCC88", "#FCA540", "#EA8223", "#D46012", "#BC3B00"],
-  neutral: "#DDDDDD",
+  neg: ["#c4cabc", "#9fb4b0", "#789fa5", "#4e8a99", "#00768d"],
+  pos: ["#c13e2e", "#ce624a", "#d88268", "#e1a287", "#e6c1a7"],
+  neutral: "#e9e0c8",
   noData: "#808B96",
 };
 

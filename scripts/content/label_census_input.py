e2638daef133416cc537a175e02a59e925e01ccd
Vivian Allen <vivian.allen@methods.co.uk>
2023-02-17 09:32:03 +0000

    refactor lable_census_input script for better performance + logging, fix metadata issues

--- scripts/content/label_census_input.py
+++ scripts/content/label_census_input.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
     input_dir = Path(sys.argv[1])
     prefix_to_match = sys.argv[2]
     content_json = sys.argv[3]
-    output_dir = input_dir.joinpath("relabelled")
-
+    output_dir = input_dir.parent.joinpath("relabelled")
+    issues_dir = input_dir.parent.joinpath("relabelled_with_issues")
     with open(content_json, "r") as f:
         content = json.load(f)
     classifications = []
@@ -17,24 +17,41 @@ if __name__ == "__main__":
             classifications.extend(v["classifications"])
 
     for input_csv in input_dir.rglob("*.csv"):
+        print(f"Relabelling fields in {input_csv}...")
         file_classification = input_csv.stem.split("_2011_2021_comparison")[0]
 
-        for c in classifications:
-            if file_classification.endswith(c["code"]):
+        issues_found = False
 
-                output_filename = output_dir.joinpath(input_csv.relative_to(input_dir))
-                output_filename.parent.mkdir(parents=True, exist_ok=True)
+        with open(input_csv) as fin:
+            reader = csv.reader(fin)
+            header = next(reader)
 
-                with open(input_csv) as fin, open(output_filename, "w") as fout:
+            matched_cls = next(
+                (c for c in classifications if file_classification.lower().endswith(c["code"].lower())), False)
+            if not matched_cls:
+                issues_found = True
+                print(f"No matching classification found for {input_csv}! Skipping")
+                continue
+            print(f"Classification {matched_cls['code']} matched with {input_csv}")
+            matched_cats = 0
+            for cat in matched_cls["categories"]:
+                for i, fieldname in enumerate(header):
+                    if fieldname.lower() == f"{prefix_to_match}{cat['name']}".lower():
+                        header[i] = cat["code"]
+                        matched_cats += 1
+                        print(
+                            f"{input_csv}: relabelling {fieldname} to {cat['code']}. {matched_cats} of {len(matched_cls['categories'])} found.")
+            if matched_cats != len(matched_cls["categories"]):
+                issues_found = True
+                print(f"Only {matched_cats} of {len(matched_cls['categories'])} found for {input_csv}")
 
-                    reader = csv.reader(fin)
-                    writer = csv.writer(fout)
+            output_filename = output_dir.joinpath(input_csv.relative_to(input_dir))
 
-                    header = next(reader)
-                    for cat in c["categories"]:
-                        for i, fieldname in enumerate(header):
-                            if fieldname == prefix_to_match + cat["name"]:
-                                header[i] = cat["code"]
+            if issues_found:
+                output_filename = issues_dir.joinpath(input_csv.relative_to(input_dir))
+            output_filename.parent.mkdir(parents=True, exist_ok=True)
 
-                    writer.writerow(header)
-                    writer.writerows(reader)
+            with open(output_filename, "w") as fout:
+                writer = csv.writer(fout)
+                writer.writerow(header)
+                writer.writerows(reader)

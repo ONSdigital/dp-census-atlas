c240f2ea3972dd9821bb0665b2a0052bb8d021ce
James Trimble <james.trimble@ons.gov.uk>
2023-02-27 12:07:51 +0000

    Update legends and redirects for living arrangements and multi religion
    To match the corrected descriptions in metadata release 45.

--- src/redirects.ts
+++ src/redirects.ts
@@ -13,8 +13,32 @@ export const redirectIfNecessary = (url: URL) => {
 // let's try to keep them in alphabetical order (by the old URL!)
 export const redirects = [
   {
+    old: "/identity/multiple-religions-in-household/hh-multi-religion-7a/multi-person-household-no-people-stated-their-religion",
+    new: "/identity/multiple-religions-in-household/hh-multi-religion-7a/multi-person-household-no-people-answered-the-religion-question",
+  },
+  {
+    old: "/identity/multiple-religions-in-household/hh-multi-religion-7a/multi-person-household-same-religion-at-least-one-person-has-stated-a-religion-but-the-household-may-include-people-who-did-not-state-their-religion",
+    new: "/identity/multiple-religions-in-household/hh-multi-religion-7a/multi-person-household-same-religion-at-least-one-person-has-stated-a-religion-but-the-household-may-include-people-who-did-not-answer-the-religion-question",
+  },
+  {
+    old: "/identity/multiple-religions-in-household/hh-multi-religion-7a/multi-person-household-no-religion-household-may-include-people-who-did-not-state-their-religion",
+    new: "/identity/multiple-religions-in-household/hh-multi-religion-7a/multi-person-household-no-religion-household-may-include-people-who-did-not-answer-the-religion-question",
+  },
+  {
+    old: "/identity/multiple-religions-in-household/hh-multi-religion-7a/multi-person-household-same-religion-and-no-religion-household-may-include-people-who-did-not-state-their-religion",
+    new: "/identity/multiple-religions-in-household/hh-multi-religion-7a/multi-person-household-same-religion-and-no-religion-household-may-include-people-who-did-not-answer-the-religion-question",
+  },
+  {
+    old: "/identity/multiple-religions-in-household/hh-multi-religion-7a/multi-person-household-at-least-two-different-religions-stated-household-may-include-people-with-no-religion-and-who-did-not-state-their-religion",
+    new: "/identity/multiple-religions-in-household/hh-multi-religion-7a/multi-person-household-at-least-two-different-religions-stated-household-may-include-people-with-no-religion-and-who-did-not-answer-the-religion-question",
+  },
+  {
     old: "/population/household-composition/hh-family-composition-4a/multiple-family-household",
     new: "/population/household-composition/hh-family-composition-4a/other-household-types",
+  },
+  {
+    old: "/population/living-arrangements/living-arrangements-11a/not-living-in-a-couple-single-never-married-and-never-registered-a-same-sex-civil-partnership",
+    new: "/population/living-arrangements/living-arrangements-11a/not-living-in-a-couple-single-never-married-and-never-registered-a-civil-partnership",
   },
   {
     old: "/population/uk-armed-forces-veteran-indicator/uk-armed-forces/previously-served-in-uk-armed-forces",

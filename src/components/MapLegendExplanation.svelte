d3fba8974b2f64b295f285200ba89cf1db440f32
petem-methods <pete.montgomery@methods.co.uk>
2023-03-08 09:41:23 +0000

    Make legend explanation improvements

--- src/components/MapLegendExplanation.svelte
+++ src/components/MapLegendExplanation.svelte
@@ -5,24 +5,22 @@
 
   export let mode: Mode;
   export let classificationCode: string;
+  export let variableName: string;
 </script>
 
 {#if mode === "change"}
   <div class="text-xs xs:text-sm pt-0.5 xs:pt-2.5">
     {#if classificationCode === "population_density"}
-      Percentage change in people per square kilometres between 2011 and 2021 census.
+      Percentage change in persons per square kilometre between 2011 and 2021 census.
     {:else if classificationCode === "median_age"}
       Change in median age between 2011 and 2021 census.
     {:else}
-      Change in
+      {variableName}: change in
       <a
         href="https://www.ons.gov.uk/peoplepopulationandcommunity/populationandmigration/populationestimates/articles/censusmaps/2022-11-02#:~:text=A%20percentage%20point%20change%20is%20the%20difference%20between%20percentages"
-        class="hyperlink-subdued mr-1">percentage points (pp)</a
-      ><span class="inline" aria-hidden="true">
-        <Icon kind="openInNew" />
-      </span>
-      for {$viz.params.variable.units}
-      between 2011 and 2021 census.
+        class="hyperlink-subdued">percentage points (pp)</a
+      >
+      for {$viz.params.variable.units} between 2011 and 2021 census.
     {/if}
   </div>
 {/if}

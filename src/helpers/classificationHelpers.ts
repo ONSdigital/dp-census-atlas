7a7184d127ff56683af2ba5e46584820849ead3c
daniel lawrence <33934706+wavemechanics@users.noreply.github.com>
2023-03-23 15:36:34 +0000

    change displayed suffix for some indicators

--- src/helpers/classificationHelpers.ts
+++ src/helpers/classificationHelpers.ts
@@ -14,6 +14,114 @@ const classificationDataDisplayConfig = (classificationCode: string) => {
       roundBreaks: (breaks: number[]) => uniqueRoundedNumbers({ numbers: breaks, decimalPlaces: 0 }),
     };
   }
+
+  const poundsClassifications = [
+    "gross_value_added_per_hour_worked",
+    "gross_median_weekly_pay",
+    "gross_disposable_household_income_per_head",
+  ]
+  if (poundsClassifications.includes(classificationCode)) {
+    return {
+      suffix: "",
+      round: (r: number) => roundNumber({ number: r, decimalPlaces: 0 }),
+      roundToString: (r: number) => r.toFixed(0),
+      roundBreaks: (breaks: number[]) => uniqueRoundedNumbers({ numbers: breaks, decimalPlaces: 0 }),
+    };
+  }
+
+  const minutesClassifications = [
+    "public_transport_or_walk_to_employment_centre_with_500_to_4999_jobs",
+    "drive_to_employment_centre_with_500_to_4999_jobs",
+    "cycle_to_employment_centre_with_500_to_4999_jobs",
+  ]
+  if (minutesClassifications.includes(classificationCode)) {
+    return {
+      suffix: "",
+      round: (r: number) => roundNumber({ number: r, decimalPlaces: 0 }),
+      roundToString: (r: number) => r.toFixed(0),
+      roundBreaks: (breaks: number[]) => uniqueRoundedNumbers({ numbers: breaks, decimalPlaces: 0 }),
+    };
+  }
+
+  const per1kClassifications = [
+    "additions_to_the_housing_stock",
+  ]
+  if (per1kClassifications.includes(classificationCode)) {
+    return {
+      suffix: "",
+      round: (r: number) => roundNumber({ number: r, decimalPlaces: 0 }),
+      roundToString: (r: number) => r.toFixed(0),
+      roundBreaks: (breaks: number[]) => uniqueRoundedNumbers({ numbers: breaks, decimalPlaces: 0 }),
+    };
+  }
+
+  const per100kClassifications = [
+    "apprenticeships_starts",
+    "apprenticeships_achievements",
+    "aged_19_years_and_over_further_education_and_skills_participation",
+    "cardiovascular_mortality_considered_preventable_in_persons_aged_under_75",
+  ]
+  if (per100kClassifications.includes(classificationCode)) {
+    return {
+      suffix: "",
+      round: (r: number) => roundNumber({ number: r, decimalPlaces: 0 }),
+      roundToString: (r: number) => r.toFixed(0),
+      roundBreaks: (breaks: number[]) => uniqueRoundedNumbers({ numbers: breaks, decimalPlaces: 0 }),
+    };
+  }
+
+  const perMillionClassifications = [
+    "homicide_offences",
+  ]
+  if (perMillionClassifications.includes(classificationCode)) {
+    return {
+      suffix: "",
+      round: (r: number) => roundNumber({ number: r, decimalPlaces: 0 }),
+      roundToString: (r: number) => r.toFixed(0),
+      roundBreaks: (breaks: number[]) => uniqueRoundedNumbers({ numbers: breaks, decimalPlaces: 0 }),
+    };
+  }
+
+  const numberClassifications = [
+    "aged_19_years_and_over_further_education_and_skills_learner_achievements",
+  ]
+  if (numberClassifications.includes(classificationCode)) {
+    return {
+      suffix: "",
+      round: (r: number) => roundNumber({ number: r, decimalPlaces: 0 }),
+      roundToString: (r: number) => r.toFixed(0),
+      roundBreaks: (breaks: number[]) => uniqueRoundedNumbers({ numbers: breaks, decimalPlaces: 0 }),
+    };
+  }
+
+  const yearsClassifications = [
+    "female_healthy_life_expectancy",
+    "male_healthy_life_expectancy",
+  ]
+  if (yearsClassifications.includes(classificationCode)) {
+    return {
+      suffix: "",
+      round: (r: number) => roundNumber({ number: r, decimalPlaces: 0 }),
+      roundToString: (r: number) => r.toFixed(0),
+      roundBreaks: (breaks: number[]) => uniqueRoundedNumbers({ numbers: breaks, decimalPlaces: 0 }),
+    };
+  }
+
+  const outOf10Classifications = [
+    "life_satisfaction",
+    "feeling_life_is_worthwhile",
+    "happiness",
+    "anxiety",
+  ]
+  if (outOf10Classifications.includes(classificationCode)) {
+    return {
+      suffix: "",
+      round: (r: number) => roundNumber({ number: r, decimalPlaces: 0 }),
+      roundToString: (r: number) => r.toFixed(0),
+      roundBreaks: (breaks: number[]) => uniqueRoundedNumbers({ numbers: breaks, decimalPlaces: 0 }),
+    };
+  }
+
   const twoDecimalPlaceClassifications = [
     "main_language_detailed",
     "main_language_detailed_23a",

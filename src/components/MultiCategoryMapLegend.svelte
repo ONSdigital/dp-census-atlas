d14d81baee7476749c6322706a66bb0a9cd8c5a7
petem-methods <pete.montgomery@methods.co.uk>
2023-03-24 11:29:30 +0000

    Make more legend improvements

--- src/components/MultiCategoryMapLegend.svelte
+++ src/components/MultiCategoryMapLegend.svelte
@@ -6,14 +6,9 @@
   import { viz } from "../stores/viz";
   import { hovered } from "../stores/hovered";
   import { selected } from "../stores/selected";
-  import { formatTemplateString } from "../helpers/categoryHelpers";
+  import { tooltip } from "../actions/tooltip";
   import { roundedClassificationDataToString, getClassificationDataSuffix } from "../helpers/classificationHelpers";
-  import { getDropdownDisplayType, getSign, shouldShowPositiveSign } from "../helpers/legendHelper";
-  import { getColours } from "../helpers/choroplethHelpers";
-  import BreaksChart from "./BreaksChart.svelte";
   import GeoTypeBadge from "./GeoTypeBadge.svelte";
-  import CategorySelector from "./CategorySelector.svelte";
-  import MapLegendExplanation from "./MapLegendExplanation.svelte";
 
   let colours = ["#3bb2d0", "#e55e5e", "#223b53", "#fbb03b", "#ccc"];
 
@@ -22,6 +17,8 @@
       .map((c, i) => ({ code: c.code, colour: colours[i] }))
       .find((c) => c.code === categoryCode).colour;
   };
+
+  const shouldBeHorizontal = () => $viz.params.categories.filter((c) => c.name.length > 25).length < 2;
 
   $: valueForHoveredGeography = $viz?.places.find((p) => p.geoCode === $hovered?.geoCode)?.categoryValues;
   $: suffix = $viz && getClassificationDataSuffix($viz.params.classification.code, $viz.params.mode);
@@ -51,7 +48,7 @@
     >
       {#if $viz?.params?.categories && active?.value !== undefined}
         <!-- full legend -->
-        <div class="flex flex-col sm:flex-row gap-2 sm:gap-4">
+        <div class="flex flex-col gap-2 sm:gap-4" class:sm:flex-row={shouldBeHorizontal()}>
           <div class="flex flex-1 flex-col">
             <div class="">
               <div class={`${legendTextClass} break-keep`}>
@@ -69,24 +66,30 @@
             {#each $viz.params.categories as category}
               {@const value = active.value.find((v) => v.code === category.code).value}
               {@const ewValue = $viz.englandAndWales[category.code]}
-              <div class="">
-                {category.name}
-                <span class="ml-0.5 font-bold">
-                  {value}%
-                </span>
+              <div class=" ">
+                <div class="leading-[1.35rem] mb-0.5" class:widows={"3"}>
+                  {category.name}&nbsp;<span class="ml-0.5 font-bold">
+                    {value}%
+                  </span>
+                </div>
                 <div class="relative h-4">
                   <div
                     class="h-4 absolute left-0 top-0"
                     style:background-color={getColourForCategory(category.code)}
                     style:width={`${value}%`}
                   />
-                  <div class="h-4 w-1 absolute left-0 top-0 bg-ons-black" style:left={`${ewValue}%`} />
+                  <div
+                    class="h-4 w-1 absolute left-0 top-0 bg-ons-black"
+                    style:left={`${Math.max(ewValue - 0.1, 0)}%`}
+                    use:tooltip={{ content: `${ewValue}% for England & Wales`, placement: "bottom" }}
+                  />
                 </div>
               </div>
             {/each}
             <div class="text-sm flex items-center gap-2 mt-4">
               <div class="h-4 w-1 bg-ons-black inline-block" />
               England and Wales average
+              <!-- <button class="hyperlink-subdued text-sm">Show values</button> -->
             </div>
           </div>
         </div>

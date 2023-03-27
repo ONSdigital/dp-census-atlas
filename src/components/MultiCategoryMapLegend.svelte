8da0908647f10dda219f7235691794c70318488e
petem-methods <pete.montgomery@methods.co.uk>
2023-03-27 14:05:26 +0100

    Improve legend adaptation, collapse

--- src/components/MultiCategoryMapLegend.svelte
+++ src/components/MultiCategoryMapLegend.svelte
@@ -9,6 +9,12 @@
   import { tooltip } from "../actions/tooltip";
   import { roundedClassificationDataToString, getClassificationDataSuffix } from "../helpers/classificationHelpers";
   import GeoTypeBadge from "./GeoTypeBadge.svelte";
+  import MultiCategoryMapLegendToggle from "./MultiCategoryMapLegendToggle.svelte";
+
+  $: open = true;
+  const toggleOpen = () => {
+    open = !open;
+  };
 
   let colours = ["#3bb2d0", "#e55e5e", "#223b53", "#fbb03b", "#ccc"];
 
@@ -18,7 +24,8 @@
       .find((c) => c.code === categoryCode).colour;
   };
 
-  const shouldBeHorizontal = () => $viz.params.categories.filter((c) => c.name.length > 25).length < 2;
+  const shouldBeHorizontal = () => $viz.params.categories.filter((c) => c.name.length > 30).length < 3;
+  const shouldBeCollapsable = () => $viz.params.categories.length > 2;
 
   $: valueForHoveredGeography = $viz?.places.find((p) => p.geoCode === $hovered?.geoCode)?.categoryValues;
   $: suffix = $viz && getClassificationDataSuffix($viz.params.classification.code, $viz.params.mode);
@@ -49,49 +56,75 @@
       {#if $viz?.params?.categories && active?.value !== undefined}
         <!-- full legend -->
         <div class="flex flex-col gap-2 sm:gap-4" class:sm:flex-row={shouldBeHorizontal()}>
-          <div class="flex flex-1 flex-col">
-            <div class="">
-              <div class={`${legendTextClass} break-keep`}>
-                <span class="font-bold">{$viz.params.variable.name}</span> in
+          <div class="flex flex-col flex-1">
+            <div class="flex gap-2 items-start justify-between">
+              <div>
+                <div class={`${legendTextClass} break-keep`}>
+                  <span class="font-bold">{$viz.params.variable.name}</span> in
+                </div>
+                <span class={legendTextClass}>
+                  {active.displayName}
+                </span>
+                <GeoTypeBadge geoType={active.geoType} />
               </div>
-              <span class={legendTextClass}>
-                {active.displayName}
-              </span>
-              <GeoTypeBadge geoType={active.geoType} />
+              <!-- {#if !shouldBeHorizontal() || !open}
+                  <MultiCategoryMapLegendToggle {open} {toggleOpen} />
+              {/if} -->
+              <div class="block" class:sm:hidden={shouldBeHorizontal() && open}>
+                <MultiCategoryMapLegendToggle {open} {toggleOpen} />
+              </div>
             </div>
             <div class="grow" />
-            <div class="text-sm">One dot represents 500 households</div>
+            {#if shouldBeHorizontal()}
+              <div class="text-sm mt-3">One dot represents 500 households</div>
+            {/if}
           </div>
-          <div class="flex flex-1 flex-col gap-2">
-            {#each $viz.params.categories as category}
-              {@const value = active.value.find((v) => v.code === category.code).value}
-              {@const ewValue = $viz.englandAndWales[category.code]}
-              <div class=" ">
-                <div class="leading-[1.35rem] mb-0.5" class:widows={"3"}>
-                  {category.name}&nbsp;<span class="ml-0.5 font-bold">
-                    {value}%
-                  </span>
+          {#if open && $viz.params.categories.length > 0}
+            <div class="flex flex-1 flex-col gap-1 sm:gap-2">
+              {#each $viz.params.categories as category, i}
+                {@const value = active.value.find((v) => v.code === category.code).value}
+                {@const ewValue = $viz.englandAndWales[category.code]}
+                <div class="">
+                  <div class="flex gap-2 items-start sm:items-end justify-between">
+                    <div class="leading-[1.35rem] md:mb-0.5" class:widows={"3"}>
+                      {category.name}&nbsp;<span class="ml-0.5 font-bold">
+                        {value}%
+                      </span>
+                    </div>
+                    {#if i === 0 && open && shouldBeCollapsable()}
+                      {#if shouldBeHorizontal()}
+                        <div class="hidden sm:block">
+                          <MultiCategoryMapLegendToggle {open} {toggleOpen} />
+                        </div>
+                      {/if}
+                    {/if}
+                  </div>
+                  <div class="relative h-4">
+                    <div
+                      class="h-4 absolute left-0 top-0"
+                      style:background-color={getColourForCategory(category.code)}
+                      style:width={`${value}%`}
+                    />
+                    <div
+                      class="h-4 w-1 absolute left-0 top-0 bg-ons-black"
+                      style:left={`${Math.max(ewValue - 0.1, 0)}%`}
+                      use:tooltip={{ content: `${ewValue}% for England & Wales`, placement: "bottom" }}
+                    />
+                  </div>
                 </div>
-                <div class="relative h-4">
-                  <div
-                    class="h-4 absolute left-0 top-0"
-                    style:background-color={getColourForCategory(category.code)}
-                    style:width={`${value}%`}
-                  />
-                  <div
-                    class="h-4 w-1 absolute left-0 top-0 bg-ons-black"
-                    style:left={`${Math.max(ewValue - 0.1, 0)}%`}
-                    use:tooltip={{ content: `${ewValue}% for England & Wales`, placement: "bottom" }}
-                  />
+              {/each}
+              <div class="flex gap-x-2 mt-2 justify-between">
+                <div class="text-sm flex items-center gap-2">
+                  <div class="h-4 w-1 bg-ons-black inline-block" />
+                  England and Wales average
+                  <!-- <button class="hyperlink-subdued text-sm">Show values</button> -->
                 </div>
+                {#if !shouldBeHorizontal()}
+                  <div class="text-sm">One dot represents 500 households</div>
+                {/if}
               </div>
-            {/each}
-            <div class="text-sm flex items-center gap-2 mt-4">
-              <div class="h-4 w-1 bg-ons-black inline-block" />
-              England and Wales average
-              <!-- <button class="hyperlink-subdued text-sm">Show values</button> -->
             </div>
-          </div>
+          {/if}
         </div>
       {:else}
         <!-- partial legend -->

b902aac562d0f1fd8ae7b8dc55643fa7aa50ed77
petem-methods <pete.montgomery@methods.co.uk>
2023-03-28 11:26:43 +0100

    Update display name of dot-density

--- src/helpers/modeHelpers.ts
+++ src/helpers/modeHelpers.ts
@@ -2,6 +2,6 @@ import type { Mode } from "../types";
 
 export const modeInfo: Record<Mode, { name: string; desc: string }> = {
   choropleth: { name: "Census 2021", desc: "Simple map of Census 2021 results" },
-  dotdensity: { name: "Dot-density", desc: "Dot-density map of Census 2021 results" },
+  dotdensity: { name: "Dot-density 2021", desc: "Dot-density map of Census 2021 results" },
   change: { name: "Change since 2011", desc: "Ten year change since Census 2011" },
 };

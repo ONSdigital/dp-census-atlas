1550f995c30cadbe701470e5a09fa85db9d667f6
petem-methods <pete.montgomery@methods.co.uk>
2023-03-29 17:16:30 +0100

    Add mode-switching test

--- e2e/homePage.spec.ts
+++ e2e/homePage.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 
-test("census maps homescreen loads", async ({ page }) => {
+test("home screen actually loads", async ({ page }) => {
   await page.goto("/");
   await expect(
     page.getByText("Use our maps to find out what people's lives were like across England and Wales in March 2021."),
